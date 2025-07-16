f6ea52b1056ec7cb0791a82397012221021b53d0
nagytommy76 <nagytommy76@gmail.com>
2025-07-16 19:49:08 +0200

    Created Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+   return (
+      <nav className='w-full h-20 mb-3 flex justify-center items-center bg-neutral-950/50 transition-colors  hover:bg-neutral-950'>
+         <h1>Navbar</h1>
+      </nav>
+   )
+}

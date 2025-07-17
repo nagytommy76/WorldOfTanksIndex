be807fe093fbf2830e85c1d041d0f6c2afad593b
nagytommy76 <nagytommy76@gmail.com>
2025-07-17 19:53:23 +0200

    Add Basic search button

---
+++ src/components/Navbar/includes/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from '@mui/material/Button'
+
+export default function SearchBar() {
+   return (
+      <section>
+         <Button variant='contained' color='primary'>
+            Search for vehicles
+         </Button>
+      </section>
+   )
+}

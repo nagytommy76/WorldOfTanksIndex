1cc7597c3d61a4b4828002dbe03f225c32e0689c
nagytommy76 <nagytommy76@gmail.com>
2025-07-17 19:53:44 +0200

    Using searchBar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from 'next/link'
+
+import SearchBar from './includes/SearchBar'
 
 export default function Navbar() {
    return (
-      <nav className='w-full h-20 mb-3 flex justify-center items-center bg-neutral-950/50 transition-colors  hover:bg-neutral-950'>
-         <h1>Navbar</h1>
-      </nav>
+      <header
+         className={'w-full h-16 mb-3 sticky top-0 bg-neutral-950/50 transition-colors  hover:bg-neutral-950'}
+      >
+         <nav className='h-full px-3 max-w-screen-2xl mx-auto flex justify-between items-center '>
+            <Link href={'/'} className={'text-2xl hover:text-amber-400'}>
+               WoT Index
+            </Link>
+            <section className={'flex flex-row gap-3'}>
+               <h1>Tech three</h1>
+               <h1>Tech three</h1>
+               <h1>Tech three</h1>
+            </section>
+            <SearchBar />
+         </nav>
+      </header>
    )
 }

5f41527c59d7780032f143166908e7ce19ca0d52
nagytommy76 <nagytommy76@gmail.com>
2025-07-19 13:06:53 +0200

    Add ThemeProvider
    Set dark theme colors, and override button text transform

---
+++ src/Providers/ThemeProvider.tsx
@@ -0,0 +1,46 @@
+'use client'
+import React from 'react'
+import { Montserrat } from 'next/font/google'
+import { createTheme, ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
+
+const montserrat = Montserrat({
+   subsets: ['latin'],
+})
+
+const darkTheme = createTheme({
+   components: {
+      MuiButton: {
+         styleOverrides: {
+            root: {
+               textTransform: 'none',
+            },
+         },
+      },
+   },
+   palette: {
+      mode: 'dark',
+      primary: {
+         main: '#fca311',
+      },
+      secondary: {
+         main: '#2b2a2a',
+      },
+      info: {
+         // main: '#357EC7',
+         main: '#0084ff',
+      },
+   },
+   typography: {
+      fontFamily: montserrat.style.fontFamily,
+      allVariants: {
+         color: '#fafafa',
+      },
+      h2: {
+         color: 'palegreen',
+      },
+   },
+})
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+   return <MUIThemeProvider theme={darkTheme}>{children}</MUIThemeProvider>
+}

09dc7928e6dd762d895abe2d31e36bffdd4e3f26
nagytommy76 <nagytommy76@gmail.com>
2025-07-20 13:40:08 +0200

    Add links
    Add social links and reference links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import Link from 'next/link'
+
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
 
 export default function Footer() {
    return (
-      <footer className='w-full h-36 flex flex-col justify-center items-center gap-1 opacity-30'>
+      <footer className='w-full h-36 flex flex-col justify-center items-center gap-1 opacity-50'>
          <h1>Made by Thomas Nagy ( nagytommy93 )</h1>
-         <div>Social links</div>
+         <div>
+            <Link href={'https://github.com/nagytommy76'} target='_blank'>
+               <GitHubIcon fontSize='large' />
+            </Link>
+            <Link href={'https://www.linkedin.com/in/tamasnagy93/'} target='_blank'>
+               <LinkedInIcon fontSize='large' />
+            </Link>
+         </div>
          <small>
             This application is not endorsed by
             <a className='hover:underline' href='https://www.wargaming.net/en' target='_blank'>
@@ -12,6 +22,13 @@ export default function Footer() {
                Wargaming.net{' '}
             </a>
             in any way.
+         </small>
+         <small>
+            The page is inspired by{' '}
+            <Link className='hover:underline' href={'https://skill4ltu.eu/'} target='_blank'>
+               skill4ltu&apos;s
+            </Link>{' '}
+            index page.
          </small>
          <small>
             World of Tanks, Wargaming.net, and all associated properties are trademarks or registered

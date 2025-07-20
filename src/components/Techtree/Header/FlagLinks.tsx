cca3439c988c8b8e90342a22d2ef08d1492f6549
nagytommy76 <nagytommy76@gmail.com>
2025-07-20 11:59:12 +0200

    add flag links
    Using usePathname to style the flags & loop over the flagSources object

---
+++ src/components/Techtree/Header/FlagLinks.tsx
@@ -0,0 +1,25 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { flagSources } from '@/Base/FlagLinks/FlagLinks'
+
+export default function FlagLinks() {
+   const pathname = usePathname()
+   return (
+      <section className={'flex flex-row justify-center gap-5'}>
+         {Object.keys(flagSources).map((key) => {
+            return (
+               <Link
+                  className={pathname === `/techtree/${key}` ? 'opacity-100' : 'opacity-50 hover:opacity-100'}
+                  href={`/techtree/${key}`}
+                  key={key}
+               >
+                  <Image src={flagSources[key].source} alt={flagSources[key].alt} width={70} height={70} />
+               </Link>
+            )
+         })}
+      </section>
+   )
+}

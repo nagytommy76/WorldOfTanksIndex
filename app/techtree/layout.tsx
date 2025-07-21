2069c29f4d555a8174dcfdf7356315b13147f25a
nagytommy76 <nagytommy76@gmail.com>
2025-07-21 19:01:18 +0200

    Add Metadata
    Add metadata with keywords

--- app/techtree/layout.tsx
+++ app/techtree/layout.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next'
 import FlagLinks from '@/componentsTechtree/Header/FlagLinks'
 import HeaderTitle from '@/componentsTechtree/Header/HeaderTitle'
+
+export const metadata: Metadata = {
+   keywords:
+      "world of tanks tech tree, world of tanks premium vehicles, world of tanks collector's vehicles, world of tanks equipment, world of tanks crews, world of tanks crew, world of tanks field modifications",
+}
 
 export default async function layout({
    children,

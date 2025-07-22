214edb474cdd8a22d34bee2624bc9dca2d99ca06
nagytommy76 <nagytommy76@gmail.com>
2025-07-22 19:55:15 +0200

    Add image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+   images: {
+      remotePatterns: [new URL('http://api.worldoftanks.eu/**')],
+   },
+}
 
-export default nextConfig;
+export default nextConfig

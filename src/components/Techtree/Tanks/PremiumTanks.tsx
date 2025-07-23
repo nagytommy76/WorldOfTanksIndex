4dda5434b2711ac951e0a2b41fed0e344326c6bd
nagytommy76 <nagytommy76@gmail.com>
2025-07-23 19:57:57 +0200

    Add Typography
    Using a div flex wrapper & map through the tiers

--- src/components/Techtree/Tanks/PremiumTanks.tsx
+++ src/components/Techtree/Tanks/PremiumTanks.tsx
@@ -1,4 +1,6 @@
 import TankCard from '@/Base/TankCard/TankCard'
+
+import Typography from '@mui/material/Typography'
 import type { ITechTreeVehicleType } from '@/types/techTreeTypes'
 
 export default function PremiumTanks({
@@ -9,10 +11,23 @@ export default function PremiumTanks({
    }
 }) {
    return (
-      <section className={'flex flex-col '}>
-         {groupedPremiumTanksByTier[8].map((premiumVehicle) => (
-            <TankCard key={premiumVehicle.tank_id} vehicle={premiumVehicle} />
-         ))}
+      <section className={'w-full flex flex-col justify-center'}>
+         <Typography variant='h6' className={'my-8 text-center text-amber-200 '}>
+            Premium vehicles
+         </Typography>
+         <div className='w-full flex flex-row flex-wrap justify-center gap-3'>
+            {Object.keys(groupedPremiumTanksByTier).map((key) => (
+               <div key={key}>
+                  {groupedPremiumTanksByTier[Number(key)].map((premiumVehicle) => (
+                     <>
+                        {!premiumVehicle.name.endsWith('FL') && (
+                           <TankCard key={premiumVehicle.tank_id} vehicle={premiumVehicle} />
+                        )}
+                     </>
+                  ))}
+               </div>
+            ))}
+         </div>
       </section>
    )
 }

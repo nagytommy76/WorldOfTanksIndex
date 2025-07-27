2e48005903b2074b31a6b62df0d134f47db9d34f
nagytommy76 <nagytommy76@gmail.com>
2025-07-27 12:22:10 +0200

    Add z index 10

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,9 @@ import DropdownMenu from './includes/Dropdown/DropdownMenu'
 export default function Navbar() {
    return (
       <header
-         className={'w-full h-16 mb-3 sticky top-0 bg-neutral-950/50 transition-colors  hover:bg-neutral-950'}
+         className={
+            'w-full h-16 mb-3 sticky top-0 bg-neutral-950/50 transition-colors  hover:bg-neutral-950 z-10'
+         }
       >
          <nav className='h-full px-5 max-w-screen-2xl mx-auto flex justify-between items-center '>
             <Link href={'/'} className={'text-2xl hover:text-amber-400'}>

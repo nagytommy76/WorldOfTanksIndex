fa09f82b0248f961473c81640fab56d04d7ae2d7
nagytommy76 <nagytommy76@gmail.com>
2025-07-27 12:27:01 +0200

    Remove code to component

--- app/techtree/germany/page.tsx
+++ app/techtree/germany/page.tsx
@@ -1,4 +1,3 @@
-import { ITechTreeVehicleType } from '@Types/techTreeTypes'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -6,55 +5,19 @@ export const metadata: Metadata = {
    description: 'Tech tree for German vehicles in World of Tanks game',
 }
 
-import PremiumTanks from '@/TechtreeTanks/PremiumTanks'
+import returnVehicles from '@/lib/getTechTreeVehicles'
 
-async function getAllGermanVehicles() {
-   const filteredData = await fetch(
-      `https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=${process.env.WOT_APP_ID}&nation=germany&fields=tank_id%2C+is_premium%2C+images%2C+type%2C+short_name%2C+name%2C+nation%2C+tier%2C+price_gold%2C+price_credit%2C+next_tanks`,
-      { method: 'GET' }
-   )
-   const response = (await filteredData.json()) as Promise<{
-      data: { [index: number]: ITechTreeVehicleType }
-   }>
-   return await response
-}
+import TechTree from '@/TechtreeTechTree'
 
 export default async function page() {
-   const allGermanVehicles = await getAllGermanVehicles()
+   const { groupedCollectorTanksByTier, groupedPremiumTanksByTier, groupedTanksByTier } =
+      await returnVehicles('germany')
 
-   const groupedTanksByTier: { [index: number]: ITechTreeVehicleType[] } = {}
-   const groupedPremiumTanksByTier: { [index: number]: ITechTreeVehicleType[] } = {}
-   const groupedCollectorTanksByTier: { [index: number]: ITechTreeVehicleType[] } = {}
-
-   Object.keys(allGermanVehicles.data).map((key) => {
-      const currentVehicle = allGermanVehicles.data[Number(key)]
-      if (currentVehicle.is_premium === true) {
-         groupedPremiumTanksByTier[currentVehicle.tier] ||= []
-         groupedPremiumTanksByTier[currentVehicle.tier].push(currentVehicle)
-      } else if (currentVehicle.price_credit !== null || currentVehicle.is_premium === false) {
-         groupedTanksByTier[currentVehicle.tier] ||= []
-         groupedTanksByTier[currentVehicle.tier].push(currentVehicle)
-      } else if (
-         currentVehicle.price_credit !== null &&
-         currentVehicle.price_gold === 0 &&
-         currentVehicle.next_tanks === null
-      ) {
-         groupedCollectorTanksByTier[currentVehicle.tier] ||= []
-         groupedCollectorTanksByTier[currentVehicle.tier].push(currentVehicle)
-      }
-   })
-
-   console.log(groupedTanksByTier[8])
-   console.log(groupedPremiumTanksByTier[8])
-   // console.log(groupedCollectorTanksByTier[4])
    return (
-      <>
-         {/* {groupedTanksByTier[10].map((techTreeVehicle) => (
-            <div key={techTreeVehicle.tank_id}>
-               <h1>{techTreeVehicle.name}</h1>
-            </div>
-         ))} */}
-         <PremiumTanks groupedPremiumTanksByTier={groupedPremiumTanksByTier} />
-      </>
+      <TechTree
+         groupedPremiumTanksByTier={groupedPremiumTanksByTier}
+         groupedTanksByTier={groupedTanksByTier}
+         groupedCollectorTanksByTier={groupedCollectorTanksByTier}
+      />
    )
 }

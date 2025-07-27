aa7347b08bb61fde03a05381db27b91450b02834
nagytommy76 <nagytommy76@gmail.com>
2025-07-27 12:27:59 +0200

    Change metadata to array

--- app/techtree/layout.tsx
+++ app/techtree/layout.tsx
@@ -1,10 +1,20 @@
 import { Metadata } from 'next'
+
 import FlagLinks from '@/componentsTechtree/Header/FlagLinks'
 import HeaderTitle from '@/componentsTechtree/Header/HeaderTitle'
+import TierList from '@/componentsTechtree/Header/TierList'
 
 export const metadata: Metadata = {
-   keywords:
-      "world of tanks tech tree, world of tanks premium vehicles, world of tanks collector's vehicles, world of tanks equipment, world of tanks crews, world of tanks crew, world of tanks field modifications",
+   keywords: [
+      'world of tanks tech tree',
+      'world of tanks premium vehicles',
+      "world of tanks collector's vehicles",
+      'world of tanks equipment',
+      'world of tanks crews',
+      'world of tanks crew',
+      'world of tanks field modifications',
+      'WoT Index page',
+   ],
 }
 
 export default async function layout({
@@ -16,6 +26,7 @@ export default async function layout({
       <section className={'min-h-screen'}>
          <HeaderTitle />
          <FlagLinks />
+         <TierList />
          {children}
       </section>
    )

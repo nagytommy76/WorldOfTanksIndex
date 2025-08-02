bdb6ed44935c67fad8b0d16bc1e6df837b5bdb8e
nagytommy76 <nagytommy76@gmail.com>
2025-08-02 10:04:08 +0200

    Working on the vehicle custom table

--- src/components/Techtree/Tanks/TechTreeTanks.tsx
+++ src/components/Techtree/Tanks/TechTreeTanks.tsx
@@ -6,15 +6,11 @@ import getIcon from '@/lib/getVehicleTypeIcon'
 import { flagSources } from '@/Base/FlagLinks/FlagLinks'
 
 import Paper from '@mui/material/Paper'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
 import Typography from '@mui/material/Typography'
 
 import type { ITechTreeVehicleType } from '@/types/techTreeTypes'
+
+const tier = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
 
 export default function TechTreeTanks({
    groupedTechTreeByTier,
@@ -24,55 +20,53 @@ export default function TechTreeTanks({
    }
 }) {
    return (
-      <TableContainer component={Paper}>
-         <Table aria-label='simple table' size='small'>
-            <TableHead>
-               <TableRow>
-                  <TableCell>Nation</TableCell>
-                  <TableCell>Type</TableCell>
-                  <TableCell>Vehicle name</TableCell>
-               </TableRow>
-            </TableHead>
-            <TableBody>
-               {Object.keys(groupedTechTreeByTier).map((key) => (
-                  <>
-                     {groupedTechTreeByTier[Number(key)].map((techTreeVehicle) => (
-                        <TableRow
-                           key={techTreeVehicle.tank_id}
-                           sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                           <Link
-                              href={`/${techTreeVehicle.tank_id}/${techTreeVehicle.name}`}
-                              key={techTreeVehicle.tank_id}
-                           >
-                              <TableCell>
-                                 <Image
-                                    src={flagSources[techTreeVehicle.nation].source}
-                                    alt={flagSources[techTreeVehicle.nation].alt}
-                                    width={30}
-                                    height={30}
-                                 />
-                              </TableCell>
-                              <TableCell>
-                                 <Image src={getIcon(techTreeVehicle.type)} alt={techTreeVehicle.name} />
-                              </TableCell>
-                              <TableCell className={'flex flex-row items-center'}>
-                                 <Image
-                                    src={techTreeVehicle.images.contour_icon}
-                                    width={65}
-                                    height={65}
-                                    alt={techTreeVehicle.name}
-                                 />
-                                 <Typography>{techTreeVehicle.name}</Typography>
-                              </TableCell>
-                           </Link>
-                        </TableRow>
-                     ))}
-                  </>
+      <section>
+         {/* <div>
+            <div>
+               <div>Nation</div>
+               <div>Type</div>
+               <div>Vehicle name</div>
+            </div>
+         </div> */}
+         {Object.keys(groupedTechTreeByTier).map((key) => (
+            <>
+               {groupedTechTreeByTier[Number(key)].map((techTreeVehicle) => (
+                  <Link
+                     className='w-full flex flex-row justify-start items-center gap-6 
+                     border-b-1 border-neutral-600 py-2 transition-all hover:bg-neutral-800'
+                     href={`/${techTreeVehicle.tank_id}/${techTreeVehicle.name}`}
+                     key={techTreeVehicle.tank_id}
+                  >
+                     <Image
+                        src={flagSources[techTreeVehicle.nation].source}
+                        alt={flagSources[techTreeVehicle.nation].alt}
+                        width={30}
+                        height={30}
+                        className='object-cover w-[35px]'
+                     />
+                     <Image
+                        src={getIcon(techTreeVehicle.type)}
+                        alt={techTreeVehicle.name}
+                        width={15}
+                        height={15}
+                        className=''
+                     />
+                     <Typography textAlign={'center'}>{tier[Number(techTreeVehicle.tier - 1)]}</Typography>
+                     <div className={'flex flex-row items-center justify-center'}>
+                        <Image
+                           src={techTreeVehicle.images.contour_icon}
+                           width={65}
+                           height={65}
+                           alt={techTreeVehicle.name}
+                        />
+                        <Typography>{techTreeVehicle.name}</Typography>
+                     </div>
+                     <Typography>{techTreeVehicle.price_credit}</Typography>
+                  </Link>
                ))}
-            </TableBody>
-         </Table>
-      </TableContainer>
+            </>
+         ))}
+      </section>
    )
    return (
       <section className={'grid grid-cols-3 grid-rows-4 gap-1'}>

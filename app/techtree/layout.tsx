47a5919c3df94ce17e17390ed8529df46f73aa00
nagytommy76 <nagytommy76@gmail.com>
2025-08-03 11:16:13 +0200

    Removed tier list

--- app/techtree/layout.tsx
+++ app/techtree/layout.tsx
@@ -2,7 +2,6 @@ import { Metadata } from 'next'
 
 import FlagLinks from '@/componentsTechtree/Header/FlagLinks'
 import HeaderTitle from '@/componentsTechtree/Header/HeaderTitle'
-import TierList from '@/componentsTechtree/Header/TierList'
 
 export const metadata: Metadata = {
    keywords: [
@@ -26,7 +25,6 @@ export default async function layout({
       <section className={'min-h-screen'}>
          <HeaderTitle />
          <FlagLinks />
-         <TierList />
          {children}
       </section>
    )

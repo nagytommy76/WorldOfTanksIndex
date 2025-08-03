64027eb6fe75eab3ba00893027c6349d6577ca41
nagytommy76 <nagytommy76@gmail.com>
2025-08-03 11:13:56 +0200

    Add tank table
    Created tech tree tanks table with basic info and link.

--- src/components/Techtree/Tanks/TechTreeTanks.tsx
+++ src/components/Techtree/Tanks/TechTreeTanks.tsx
@@ -1,16 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import TankCard from '@/Base/TankCard/TankCard'
 
 import getIcon from '@/lib/getVehicleTypeIcon'
 import { flagSources } from '@/Base/FlagLinks/FlagLinks'
 
-import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 import type { ITechTreeVehicleType } from '@/types/techTreeTypes'
-
-const tier = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+import tiers from '@/lib/tierList'
 
 export default function TechTreeTanks({
    groupedTechTreeByTier,
@@ -20,39 +18,51 @@ export default function TechTreeTanks({
    }
 }) {
    return (
-      <section>
-         {/* <div>
-            <div>
-               <div>Nation</div>
-               <div>Type</div>
-               <div>Vehicle name</div>
-            </div>
-         </div> */}
+      <section className='w-3xl mx-auto my-11'>
+         <div className='w-full my-3 flex flex-row justify-start items-center gap-6'>
+            <Typography className='w-[50px]' variant='subtitle1'>
+               Nation
+            </Typography>
+            <Typography className='w-[30px]' variant='subtitle1'>
+               Type
+            </Typography>
+            <Typography variant='subtitle1'>Tier</Typography>
+            <Typography className='w-[250px]' variant='subtitle1'>
+               Vehicle name
+            </Typography>
+            <Typography variant='subtitle1'>Credits</Typography>
+         </div>
          {Object.keys(groupedTechTreeByTier).map((key) => (
-            <>
+            <div key={key} id={`tier-${key}`}>
                {groupedTechTreeByTier[Number(key)].map((techTreeVehicle) => (
                   <Link
+                     id={techTreeVehicle.tank_id.toString()}
                      className='w-full flex flex-row justify-start items-center gap-6 
                      border-b-1 border-neutral-600 py-2 transition-all hover:bg-neutral-800'
                      href={`/${techTreeVehicle.tank_id}/${techTreeVehicle.name}`}
                      key={techTreeVehicle.tank_id}
                   >
-                     <Image
-                        src={flagSources[techTreeVehicle.nation].source}
-                        alt={flagSources[techTreeVehicle.nation].alt}
-                        width={30}
-                        height={30}
-                        className='object-cover w-[35px]'
-                     />
-                     <Image
-                        src={getIcon(techTreeVehicle.type)}
-                        alt={techTreeVehicle.name}
-                        width={15}
-                        height={15}
-                        className=''
-                     />
-                     <Typography textAlign={'center'}>{tier[Number(techTreeVehicle.tier - 1)]}</Typography>
-                     <div className={'flex flex-row items-center justify-center'}>
+                     <div className='w-[50px] flex flex-row justify-center' title={techTreeVehicle.nation}>
+                        <Image
+                           src={flagSources[techTreeVehicle.nation].source}
+                           alt={flagSources[techTreeVehicle.nation].alt}
+                           width={90}
+                           height={90}
+                           className='object-cover w-[45px]'
+                        />
+                     </div>
+                     <div className='w-[30px] flex flex-row justify-center' title={techTreeVehicle.type}>
+                        <Image
+                           src={getIcon(techTreeVehicle.type)}
+                           alt={techTreeVehicle.name}
+                           width={15}
+                           height={15}
+                        />
+                     </div>
+                     <Typography className='w-[30px] text-center' textAlign={'center'}>
+                        {tiers[Number(techTreeVehicle.tier - 1)]}
+                     </Typography>
+                     <div className={'w-[250px] flex flex-row items-center '}>
                         <Image
                            src={techTreeVehicle.images.contour_icon}
                            width={65}
@@ -61,24 +71,14 @@ export default function TechTreeTanks({
                         />
                         <Typography>{techTreeVehicle.name}</Typography>
                      </div>
-                     <Typography>{techTreeVehicle.price_credit}</Typography>
+                     <Typography className='min-w-[80px]'>
+                        {techTreeVehicle.price_credit?.toLocaleString()}
+                     </Typography>
+                     <Button>More details</Button>
                   </Link>
                ))}
-            </>
+            </div>
          ))}
-      </section>
-   )
-   return (
-      <section className={'grid grid-cols-3 grid-rows-4 gap-1'}>
-         {/* <div> */}
-         {Object.keys(groupedTechTreeByTier).map((key) => (
-            <Paper key={key} className='my-4 w-125 h-125 grid col-span-2'>
-               {groupedTechTreeByTier[Number(key)].map((techTreeVehicle) => (
-                  <TankCard key={techTreeVehicle.tank_id} vehicle={techTreeVehicle} />
-               ))}
-            </Paper>
-         ))}
-         {/* </div> */}
       </section>
    )
 }

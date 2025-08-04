9c1a31bbad69a6ad9530148a7805bd9da6c0622f
nagytommy76 <nagytommy76@gmail.com>
2025-08-04 19:36:01 +0200

    Use getIcon function

--- src/components/Base/TankCard/TankCard.tsx
+++ src/components/Base/TankCard/TankCard.tsx
@@ -3,33 +3,12 @@ import Image from 'next/image'
 
 import type { ITechTreeVehicleType } from '@/types/techTreeTypes'
 
-import ATSPG from '@/ImagesTankIcons/AT-SPG.svg'
-import heavyTank from '@/ImagesTankIcons/heavyTank.svg'
-import lightTank from '@/ImagesTankIcons/lightTank.svg'
-import mediumTank from '@/ImagesTankIcons/mediumTank.svg'
-import SPG from '@/ImagesTankIcons/SPG.svg'
-
-function getIcon(type: string) {
-   switch (type) {
-      case 'AT-SPG':
-         return ATSPG
-      case 'SPG':
-         return SPG
-      case 'heavyTank':
-         return heavyTank
-      case 'mediumTank':
-         return mediumTank
-      case 'lightTank':
-         return lightTank
-      default:
-         return ''
-   }
-}
+import getIcon from '@/lib/getVehicleTypeIcon'
 
 export default function TankCard({ vehicle }: { vehicle: ITechTreeVehicleType }) {
    return (
       <div className='w-[140px] h-[40px]'>
-         <Link href={`/${vehicle.tank_id}/${vehicle.name}`} title={vehicle.name} className={'w-full h-full'}>
+         <Link href={`/${vehicle.tank_id}/${vehicle.tag}`} title={vehicle.name} className={'w-full h-full'}>
             <div className='relative w-full h-full'>
                <Image
                   className={`w-full h-full object-cover ${vehicle.type}`}

be5572721db538eca51b082a5af47be29d103f3e
nagytommy76 <nagytommy76@gmail.com>
2025-08-04 19:36:32 +0200

    Add LinkComponent with hover underline

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,32 +3,39 @@ import Link from 'next/link'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 
+function LinkComponent({ href, children }: { href: string; children: React.ReactNode }) {
+   return (
+      <Link target='_blank' className='hover:underline' href={href}>
+         {children}
+      </Link>
+   )
+}
+
 export default function Footer() {
    return (
       <footer className='w-full h-36 flex flex-col justify-center items-center gap-1 opacity-50'>
-         <h1>Made by Thomas Nagy ( nagytommy93 )</h1>
+         <h1>
+            Made by Thomas Nagy{' '}
+            <LinkComponent href={'https://tomato.gg/stats/nagytommy93-511400957/EU?tab=main'}>
+               ( nagytommy93 )
+            </LinkComponent>
+         </h1>
          <div>
-            <Link href={'https://github.com/nagytommy76'} target='_blank'>
+            <LinkComponent href={'https://github.com/nagytommy76'}>
                <GitHubIcon fontSize='large' />
-            </Link>
-            <Link href={'https://www.linkedin.com/in/tamasnagy93/'} target='_blank'>
+            </LinkComponent>
+            <LinkComponent href={'https://www.linkedin.com/in/tamasnagy93/'}>
                <LinkedInIcon fontSize='large' />
-            </Link>
+            </LinkComponent>
          </div>
          <small>
             This application is not endorsed by
-            <a className='hover:underline' href='https://www.wargaming.net/en' target='_blank'>
-               {' '}
-               Wargaming.net{' '}
-            </a>
+            <LinkComponent href='https://www.wargaming.net/en'> Wargaming.net </LinkComponent>
             in any way.
          </small>
          <small>
             The page is inspired by{' '}
-            <Link className='hover:underline' href={'https://skill4ltu.eu/'} target='_blank'>
-               skill4ltu&apos;s
-            </Link>{' '}
-            index page.
+            <LinkComponent href={'https://skill4ltu.eu/'}>skill4ltu&apos;s</LinkComponent> index page.
          </small>
          <small>
             World of Tanks, Wargaming.net, and all associated properties are trademarks or registered

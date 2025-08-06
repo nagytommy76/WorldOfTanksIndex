31a6dcb29da490a0897ea0d43cc7f8e7b64c4f37
nagytommy76 <nagytommy76@gmail.com>
2025-08-06 19:41:13 +0200

    Add vehicle tag not name

--- src/components/Techtree/Tanks/TechTreeTanks.tsx
+++ src/components/Techtree/Tanks/TechTreeTanks.tsx
@@ -39,7 +39,7 @@ export default function TechTreeTanks({
                      id={techTreeVehicle.tank_id.toString()}
                      className='w-full flex flex-row justify-start items-center gap-6 
                      border-b-1 border-neutral-600 py-2 transition-all hover:bg-neutral-800'
-                     href={`/${techTreeVehicle.tank_id}/${techTreeVehicle.name}`}
+                     href={`/${techTreeVehicle.tank_id}/${techTreeVehicle.tag}`}
                      key={techTreeVehicle.tank_id}
                   >
                      <div className='w-[50px] flex flex-row justify-center' title={techTreeVehicle.nation}>

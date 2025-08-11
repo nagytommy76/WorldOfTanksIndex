d1cb7a19a7f008f249bbb99ae4a1a30a5354ef8d
nagytommy76 <nagytommy76@gmail.com>
2025-08-11 19:49:54 +0200

    Working on the modules tree

--- src/components/VehicleDetails/Modules.tsx
+++ src/components/VehicleDetails/Modules.tsx
@@ -14,21 +14,31 @@ export default async function Modules({
       }
    }
 }) {
+   let stage = 0
    const moduleTree: { [stage: string]: any } = {}
+   // const moduleTree: any = []
 
-   for (const [key, module] of Object.entries(modulesTree[Number(tank_id)].modules_tree)) {
-      // moduleTree.push(module)
-      // console.log('Key: ', key, 'Module: ', module)
-      if (module.is_default) {
-         moduleTree[module.type] ||= []
-         moduleTree[module.type].push(module)
-         console.log(module)
-      }
-
-      // console.log(moduleTree)
+   function recursion(module: any) {
+      moduleTree[module.type] ||= []
+      moduleTree[module.type].push(module)
+      if (module.next_modules !== null) {
+         if (module.next_modules.length > 1) {
+            module.next_modules.map((moduleId: number) => {
+               console.log(moduleId)
+            })
+         } else {
+            recursion(modulesTree[Number(tank_id)].modules_tree[module.next_modules[0]])
+         }
+      } else stage++
    }
 
-   // console.log(modulesTree[Number(tank_id)].modules_tree)
+   for (const [key, module] of Object.entries(modulesTree[Number(tank_id)].modules_tree)) {
+      if (module.is_default) {
+         recursion(module)
+      }
+   }
+   console.log(moduleTree)
+
    return (
       <div>
          <h1>Modules</h1>

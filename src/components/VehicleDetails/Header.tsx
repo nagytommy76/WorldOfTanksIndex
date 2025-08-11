f8a58e0c1ea15132bc86784c7852689213f0c5e5
nagytommy76 <nagytommy76@gmail.com>
2025-08-11 19:50:17 +0200

    Add -9 z-index

--- src/components/VehicleDetails/Header.tsx
+++ src/components/VehicleDetails/Header.tsx
@@ -13,7 +13,7 @@ export default function Header({
    return (
       <header className='min-h-[600px] relative'>
          <Image
-            className='absolute top-0 left-0'
+            className='absolute top-0 left-0 -z-9'
             src={`https://eu-wotp.wgcdn.co/dcont/tankopedia_images/${tank_name.toLocaleLowerCase()}/${tank_name.toLocaleLowerCase()}_image.png`}
             alt={tank_name}
             width={1500}

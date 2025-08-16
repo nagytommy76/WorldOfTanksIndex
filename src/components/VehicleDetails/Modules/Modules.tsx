344eb521243ea4cd00137a12efae8aa56e5849b4
nagytommy76 <nagytommy76@gmail.com>
2025-08-16 11:01:19 +0200

    Using ReturnModuleImg & ReturnModuleType

--- src/components/VehicleDetails/Modules/Modules.tsx
+++ src/components/VehicleDetails/Modules/Modules.tsx
@@ -1,33 +1,13 @@
 import Image from 'next/image'
-import { IModules, ModuleType } from '@/types/VehicleDetails/module'
+import ReturnModuleImg from './Includes/ReturnModuleImg'
+import type { IModules, ModuleType } from '@/types/VehicleDetails/module'
 
 import List from '@mui/material/List'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Typography from '@mui/material/Typography'
 
-import vehicleChassis from '@/ImagesVehicleParts/vehiclechassis.png'
-import vehicleEngine from '@/ImagesVehicleParts/vehicleengine.png'
-import vehicleGun from '@/ImagesVehicleParts/vehiclegun.png'
-import vehicleRadio from '@/ImagesVehicleParts/vehicleradio.png'
-import vehicleTurret from '@/ImagesVehicleParts/vehicleturret.png'
-
-function ReturnModuleType({ moduleType }: { moduleType: ModuleType }) {
-   switch (moduleType) {
-      case 'vehicleChassis':
-         return <h1>Chassis</h1>
-      case 'vehicleEngine':
-         return <h1>Engines</h1>
-      case 'vehicleGun':
-         return <h1>Guns</h1>
-      case 'vehicleRadio':
-         return <h1>Radios</h1>
-      case 'vehicleTurret':
-         return <h1>Turrets</h1>
-      default:
-         return <h1></h1>
-   }
-}
+import ReturnModuleType from './Includes/ModuleType'
 
 export default async function Modules({
    modulesTree,
@@ -76,22 +56,7 @@ export default async function Modules({
                      <ListItemButton id={module.module_id.toString()} key={module.module_id}>
                         <ListItemAvatar>
                            <Image
-                              src={(function () {
-                                 switch (module.type) {
-                                    case 'vehicleChassis':
-                                       return vehicleChassis
-                                    case 'vehicleEngine':
-                                       return vehicleEngine
-                                    case 'vehicleGun':
-                                       return vehicleGun
-                                    case 'vehicleRadio':
-                                       return vehicleRadio
-                                    case 'vehicleTurret':
-                                       return vehicleTurret
-                                    default:
-                                       return '' // Return a default image source
-                                 }
-                              })()}
+                              src={ReturnModuleImg(module.type)}
                               alt={module.name}
                               width={45}
                               height={45}

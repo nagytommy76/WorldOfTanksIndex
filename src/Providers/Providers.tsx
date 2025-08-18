5801d21c1e43107768f33bf6a31035a8bbbfebd1
nagytommy76 <nagytommy76@gmail.com>
2025-08-18 12:49:12 +0200

    Add Providers
    Created Providers file to add query,theme @ mui providers

---
+++ src/Providers/Providers.tsx
@@ -0,0 +1,28 @@
+'use client'
+import getQueryClient from './queryClient'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClientProvider } from '@tanstack/react-query'
+
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import ThemeProvider from '@/ProvidersThemeProvider'
+
+export default function Providers({
+   children,
+}: Readonly<{
+   children: React.ReactNode
+}>) {
+   // NOTE: Avoid useState when initializing the query client if you don't
+   //       have a suspense boundary between this and the code that may
+   //       suspend because React will throw away the client on the initial
+   //       render if it suspends and there is no boundary
+   const queryClient = getQueryClient()
+
+   return (
+      <QueryClientProvider client={queryClient}>
+         <ReactQueryDevtools initialIsOpen={false} />
+         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider>{children}</ThemeProvider>
+         </AppRouterCacheProvider>
+      </QueryClientProvider>
+   )
+}

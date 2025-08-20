b8363adc387f7b246bfcd8605af275d9fbde3851
nagytommy76 <nagytommy76@gmail.com>
2025-08-20 14:18:05 +0200

    Add actions
    Add SET_SELECTED_MODULE_IDS & SET_MODULE_ID_BY_TYPE

--- src/components/VehicleDetails/Context/ModuleReducer.tsx
+++ src/components/VehicleDetails/Context/ModuleReducer.tsx
@@ -1,14 +1,26 @@
-import type { IModuleAction, IModulesReducerState } from './Types'
+import type { IModuleContextActions, IModulesReducerState } from './Types'
 
-export default function ModuleReducer(state: IModulesReducerState, { payload, type }: IModuleAction) {
+export default function ModuleReducer(state: IModulesReducerState, { payload, type }: IModuleContextActions) {
    switch (type) {
       case 'SET_MODULE_GROUP':
          return {
             ...state,
             moduleGroup: payload,
          }
-
-      default:
-         break
+      case 'SET_SELECTED_MODULE_IDS':
+         return {
+            ...state,
+            selectedModuleIds: payload,
+         }
+      case 'SET_MODULE_ID_BY_TYPE':
+         return {
+            ...state,
+            selectedModuleIds: { ...state.selectedModuleIds, [payload.type]: payload.value },
+         }
+      default: {
+         return {
+            ...state,
+         }
+      }
    }
 }

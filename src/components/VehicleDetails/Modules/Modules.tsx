f0d061920af78b47699b89259647274164022d41
nagytommy76 <nagytommy76@gmail.com>
2025-08-20 14:16:49 +0200

    Using ModuleSelect & DetailsTable component

--- src/components/VehicleDetails/Modules/Modules.tsx
+++ src/components/VehicleDetails/Modules/Modules.tsx
@@ -1,16 +1,10 @@
 'use client'
-import Image from 'next/image'
-import ReturnModuleImg from './Includes/ReturnModuleImg'
-import type { IModules, ModuleType } from '@/types/VehicleDetails/module'
+import type { IModules } from '@/types/VehicleDetails/module'
 
-import useGroupModules from './Hooks/useGroupModules'
+import ModuleContextProvider from '../Context/ModuleContext'
 
-import List from '@mui/material/List'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import Typography from '@mui/material/Typography'
-
-import ReturnModuleType from './Includes/ModuleType'
+import ModuleSelect from './ModuleSelect/ModuleSelect'
+import DetailsTable from './DetailsTable/DetailsTable'
 
 export default function Modules({
    modulesTree,
@@ -19,43 +13,17 @@ export default function Modules({
    tank_id: string
    modulesTree: { [module_id: number]: IModules }
 }) {
-   const { moduleTree } = useGroupModules(modulesTree)
+   // const { moduleTree } = useGroupModules(modulesTree)
 
    // https://api.worldoftanks.eu/wot/encyclopedia/vehicleprofile/?application_id=97f4b2c203d63f5db6fd508661fe5ba
    // 8&tank_id=4113&suspension_id=8466&engine_id=4885&gun_id=2068&turret_id=6675&radio_id=2071
 
    return (
-      <section className={'w-full min-h-[600px] my-20 flex flex-row justify-between gap-5'}>
-         <aside>
-            <Typography variant='h5'>Modules</Typography>
-            {Object.entries(moduleTree).map(([key, modules]) => (
-               <List key={key} sx={{ width: '100%', maxWidth: 290 }}>
-                  <ReturnModuleType moduleType={key as ModuleType} />
-                  {modules.map((module) => (
-                     <ListItemButton
-                        id={module.module_id.toString()}
-                        key={module.module_id}
-                        selected={
-                           module === moduleTree[module.type][moduleTree[module.type].length - 1] && true
-                        }
-                     >
-                        <ListItemAvatar>
-                           <Image
-                              src={ReturnModuleImg(module.type)}
-                              alt={module.name}
-                              width={45}
-                              height={45}
-                           />
-                        </ListItemAvatar>
-                        <Typography variant='caption'>{module.name}</Typography>
-                     </ListItemButton>
-                  ))}
-               </List>
-            ))}
-         </aside>
-         <section className='w-full'>
-            <h1>Details list</h1>
+      <ModuleContextProvider modulesTree={modulesTree} tank_id={tank_id}>
+         <section className={'w-full min-h-[600px] my-20 flex flex-row justify-between gap-5'}>
+            <ModuleSelect />
+            <DetailsTable />
          </section>
-      </section>
+      </ModuleContextProvider>
    )
 }

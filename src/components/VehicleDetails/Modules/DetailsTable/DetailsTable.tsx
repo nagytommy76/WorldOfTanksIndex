9e9ecb161642359926d11faf9276a3096e38c56b
nagytommy76 <nagytommy76@gmail.com>
2025-08-23 12:12:15 +0200

    Using Firepower & clean up

--- src/components/VehicleDetails/Modules/DetailsTable/DetailsTable.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/DetailsTable.tsx
@@ -1,44 +1,17 @@
 'use client'
-import { useContext } from 'react'
-import { DetailsContext } from '@/DetailsContext/DetailsContext'
 
 import Typography from '@mui/material/Typography'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 
-export default function DetailsTable() {
-   const {
-      vehicleProfileReducer: { vehicleProfile },
-   } = useContext(DetailsContext)
-   if (!vehicleProfile) return null
+import Firepower from './Includes/Firepower'
 
+export default function DetailsTable() {
    return (
       <section className='w-full'>
          <Typography variant='h5'>Tank specifications</Typography>
          <TableContainer className='w-1/2' component={Paper}>
-            <Table aria-label='simple table'>
-               <TableHead>
-                  <TableRow>
-                     <TableCell>Firepower</TableCell>
-                     <TableCell align='right'>icon</TableCell>
-                  </TableRow>
-               </TableHead>
-               <TableBody>
-                  <TableRow key={'s'} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                     <TableCell component='th' scope='row'>
-                        <Typography variant='caption'>Average Damage</Typography>
-                     </TableCell>
-                     <TableCell align='right'>
-                        <Typography variant='caption'>{vehicleProfile.ammo[0]?.damage[1]} HP</Typography>
-                     </TableCell>
-                  </TableRow>
-               </TableBody>
-            </Table>
+            <Firepower />
          </TableContainer>
       </section>
    )

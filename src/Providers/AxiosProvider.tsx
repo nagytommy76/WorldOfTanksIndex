0f5635745da1aa2f91e6c6ade67d515fea3b9b64
nagytommy76 <nagytommy76@gmail.com>
2025-08-27 13:19:53 +0200

    Changed base url
    To: http://localhost:3000/api

--- src/Providers/AxiosProvider.tsx
+++ src/Providers/AxiosProvider.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const axiosInstance = axios.create({
-   baseURL: 'https://api.worldoftanks.eu/wot',
+   // baseURL: 'https://api.worldoftanks.eu/wot',
+   baseURL: 'http://localhost:3000/api',
    headers: {
       'Content-Type': 'application/json',
    },

f07737ac67c1f76d63bc423b1b091fabe475312c
nagytommy76 <nagytommy76@gmail.com>
2025-08-28 14:29:09 +0200

    Add request
    Add fetch to get the next buildId

--- app/api/[tank_id]/[tank_name]/route.ts
+++ app/api/[tank_id]/[tank_name]/route.ts
@@ -5,26 +5,28 @@ export async function GET(
    { params }: { params: Promise<{ tank_id: string; tank_name: string }> }
 ) {
    const { tank_id, tank_name } = await params
-   const url = `https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tanks/${tank_id}/${tank_name}/EU.json`
-   console.log(await params)
+   // const url = `https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tanks/${tank_id}/${tank_name}/EU.json`
+   // const url = `https://tomato.gg/_next/data/yxdJe2YiBXUm30Jk6B22f/en/tanks/${tank_id}/${tank_name}/EU.json`
    try {
-      if (tank_id && tank_name) {
-         //  console.log(tankId, tankName)
-         //  console.log('CSASÉÁLÉSDSDGÉSDG')
-      }
-      const res = await fetch(url, {
-         headers: {
-            'User-Agent': 'Mozilla/5.0', // optional, helps avoid being blocked
-         },
-      })
+      const result = await fetch(`https://tomato.gg/_next/data/en/tanks/${tank_id}/${tank_name}/EU.json`)
+      const html = await result.text()
+      const match = html.match(/<script id="__NEXT_DATA__" type="application\/json">(.*?)<\/script>/)
 
-      if (!res.ok) {
-         return NextResponse.json({ error: 'Failed to fetch data' }, { status: res.status })
+      if (match) {
+         const json = JSON.parse(match[1])
+         const res = await fetch(
+            `https://tomato.gg/_next/data/${json.buildId}/en/tanks/${tank_id}/${tank_name}/EU.json`,
+            {
+               headers: {
+                  'User-Agent': 'Mozilla/5.0', // optional, helps avoid being blocked
+               },
+            }
+         )
+         const data = await res.json()
+         return NextResponse.json(data.pageProps)
       }
-
-      const data = await res.json()
-      return NextResponse.json(data)
+      return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 })
    } catch (err) {
-      return NextResponse.json({ error: 'Fetch error', details: (err as Error).message }, { status: 500 })
+      return NextResponse.json({ error: err, details: (err as Error).message }, { status: 500 })
    }
 }

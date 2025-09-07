49b5a13377864b61905824d462c41e162c4df27a
nagytommy76 <nagytommy76@gmail.com>
2025-09-07 12:23:13 +0200

    Using reducer
    Using tomato reducer & add the selection hooks

--- src/components/VehicleDetails/Context/TomatoContext/TomatoContext.tsx
+++ src/components/VehicleDetails/Context/TomatoContext/TomatoContext.tsx
@@ -1,13 +1,41 @@
-import { createContext } from 'react'
-import useGetTomatoTankStats from '../Hooks/useGetTomatoTankStats'
-import type { ITomatoTankStats, ITankData } from '@/types/VehicleDetails/tomatoGGTankStats'
+import { createContext, useReducer } from 'react'
+import TomatoReducer from './TomatoReducer'
 
-export const TomatoContext = createContext<ITomatoTankStats>({
+import type { ITankData } from '@/types/VehicleDetails/tomatoGGTankStats'
+import { ITomatoContext, tomatoInitialState } from './Types'
+
+import useGetTomatoTankStats from '../Hooks/useGetTomatoTankStats'
+import useSetChassis from './Hooks/useSetChassis'
+import useSetRadios from './Hooks/useSetRadios'
+import useSetGuns from './Hooks/useSetGuns'
+import useSetTurrets from './Hooks/useSetTurrets'
+import useSetEngines from './Hooks/useSetEngines'
+
+export const TomatoContext = createContext<ITomatoContext>({
+   tomatoDispatch: () => null,
+   tomatoReducer: tomatoInitialState,
    tankData: {} as ITankData,
 })
 
 export default function TomatoContextProvider({ children }: { children: React.ReactNode }) {
+   const [tomatoReducer, tomatoDispatch] = useReducer(TomatoReducer, tomatoInitialState)
    const tankData = useGetTomatoTankStats()
 
-   return <TomatoContext.Provider value={tankData}>{children}</TomatoContext.Provider>
+   useSetChassis(tankData.tankData, tomatoDispatch)
+   useSetRadios(tankData.tankData, tomatoDispatch)
+   useSetGuns(tankData.tankData, tomatoDispatch)
+   useSetTurrets(tankData.tankData, tomatoDispatch)
+   useSetEngines(tankData.tankData, tomatoDispatch)
+
+   return (
+      <TomatoContext.Provider
+         value={{
+            tankData: tankData.tankData,
+            tomatoReducer,
+            tomatoDispatch,
+         }}
+      >
+         {children}
+      </TomatoContext.Provider>
+   )
 }

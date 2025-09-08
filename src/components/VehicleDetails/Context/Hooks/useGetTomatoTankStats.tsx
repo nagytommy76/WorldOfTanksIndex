a746e0699b200dbc8fb32481e1f15a73a3724dc6
nagytommy76 <nagytommy76@gmail.com>
2025-09-08 20:18:19 +0200

    Using tankid, name props

--- src/components/VehicleDetails/Context/Hooks/useGetTomatoTankStats.tsx
+++ src/components/VehicleDetails/Context/Hooks/useGetTomatoTankStats.tsx
@@ -1,16 +1,14 @@
 import axios from '@/ProvidersAxiosProvider'
 import { useQuery } from '@tanstack/react-query'
 
-import { useContext, useState, useEffect } from 'react'
-import { ModuleContext } from '@/ModuleContext/ModuleContext'
+import { useState, useEffect } from 'react'
 
 import type { ITomatoTankStats } from '@/types/VehicleDetails/tomatoGGTankStats'
 
 // https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tanks/9745/e-75/EU.json?tankId=9745&name=e-75&server=EU
 // https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tank-stats.json
 
-export default function useGetTomatoTankStats() {
-   const { tank_short_name, tank_id } = useContext(ModuleContext)
+export default function useGetTomatoTankStats(tank_short_name: string, tank_id: string) {
    const [tomatoTankStats, setTomatoTankStats] = useState<ITomatoTankStats>({} as ITomatoTankStats)
    async function getTomatoTankStats() {
       const response = (await axios.get(`/${tank_id}/${tank_short_name}`)) as { data: ITomatoTankStats }

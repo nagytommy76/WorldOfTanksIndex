23939cdbdb96769a6066aa1296c6707dc01d5f41
nagytommy76 <nagytommy76@gmail.com>
2025-09-10 19:23:01 +0200

    Remove hook
    Removed default selected module names hook -> set in module set

--- src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
@@ -12,18 +12,13 @@ import Typography from '@mui/material/Typography'
 import ReturnModuleImg from '../Includes/ReturnModuleImg'
 import ReturnModuleType from '../Includes/ModuleType'
 
-import useSetDefaultSelectedModuleNames from '../Hooks/useSetDefaultSelectedModuleNames'
-
 export default function ModuleSelect() {
    const {
       tomatoReducer: { selectedModuleNames, moduleGroup },
       tomatoDispatch,
    } = useContext(TomatoContext)
 
-   useSetDefaultSelectedModuleNames()
-
    function setModuleNameByType(moduleType: ModuleType, moduleName: string) {
-      console.log(moduleName, moduleType)
       tomatoDispatch({
          type: 'SET_MODULE_NAME_BY_TYPE',
          payload: { type: moduleType, value: moduleName },

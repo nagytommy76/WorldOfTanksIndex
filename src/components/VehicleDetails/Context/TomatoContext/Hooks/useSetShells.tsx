1cbc3484762a86c0a1c40cbf54dcfabb0c59306f
nagytommy76 <nagytommy76@gmail.com>
2025-09-14 10:35:17 +0200

    Add useSetShells
    Set the shell module group object & default shell -> always 1st shell

---
+++ src/components/VehicleDetails/Context/TomatoContext/Hooks/useSetShells.tsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect } from 'react'
+import { TomatoContext } from '@/TomatoContext/TomatoContext'
+import type { IShells } from '@/types/VehicleDetails/tomatoGGTankStats'
+
+export default function useSetShells() {
+   const {
+      tomatoReducer: {
+         moduleGroup: { vehicleTurret },
+         selectedModuleNames: { vehicleTurret: selectedTurretName, vehicleGun: selectedGun },
+      },
+      tomatoDispatch,
+   } = useContext(TomatoContext)
+
+   useEffect(() => {
+      const foundSelectedTurret = Object.values(vehicleTurret).find(
+         (turret) => turret.name === selectedTurretName
+      )
+      const foundSelectedGun = foundSelectedTurret?.guns.find((gun) => gun.name === selectedGun)
+
+      if (foundSelectedTurret && foundSelectedGun) {
+         const helperObjectShells: { [shellName: string]: IShells } = {}
+         for (const shell of foundSelectedGun?.shells || []) {
+            helperObjectShells[shell.name] = shell
+         }
+         tomatoDispatch({
+            type: 'SET_SHELLS',
+            payload: helperObjectShells,
+         })
+
+         const vehiclShellsKeys: string[] = Object.keys(helperObjectShells)
+         tomatoDispatch({
+            type: 'SET_MODULE_NAME_BY_TYPE',
+            payload: {
+               type: 'shells',
+               value: helperObjectShells[vehiclShellsKeys[0]]?.name || '',
+            },
+         })
+      }
+   }, [selectedTurretName, selectedGun, vehicleTurret, tomatoDispatch])
+
+   return null
+}

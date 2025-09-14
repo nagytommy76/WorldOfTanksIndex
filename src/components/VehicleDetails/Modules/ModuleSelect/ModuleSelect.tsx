afc9a58be48d8c044c5dfe3b5ac245e568765c3e
nagytommy76 <nagytommy76@gmail.com>
2025-09-14 10:39:29 +0200

    Use ModuleImage component

--- src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
@@ -1,16 +1,14 @@
 'use client'
-import Image from 'next/image'
 import { useContext } from 'react'
 import { TomatoContext } from '@/TomatoContext/TomatoContext'
-import type { ModuleType } from '@/types/VehicleDetails/module'
+import type { AmmoType, ModuleType } from '@/types/VehicleDetails/module'
 
 import List from '@mui/material/List'
 import ListItemButton from '@mui/material/ListItemButton'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Typography from '@mui/material/Typography'
 
-import ReturnModuleImg from '../Includes/ReturnModuleImg'
 import ReturnModuleType from '../Includes/ModuleType'
+import ModuleImage from '../Includes/ModuleImage'
 
 export default function ModuleSelect() {
    const {
@@ -31,20 +29,17 @@ export default function ModuleSelect() {
          {Object.entries(moduleGroup).map(([key, modules]) => (
             <List key={key} sx={{ width: '100%', maxWidth: 290 }}>
                <ReturnModuleType moduleType={key as ModuleType} />
-               {Object.values(modules as Record<string, { name: string }>).map((module) => (
+               {Object.values(modules as Record<string, { name: string; image?: AmmoType }>).map((module) => (
                   <ListItemButton
                      key={module.name}
                      selected={module.name === selectedModuleNames[key as ModuleType]}
                      onClick={() => setModuleNameByType(key as ModuleType, module.name)}
                   >
-                     <ListItemAvatar>
-                        <Image
-                           src={ReturnModuleImg(key as ModuleType)}
-                           alt={module.name}
-                           width={45}
-                           height={45}
-                        />
-                     </ListItemAvatar>
+                     <ModuleImage
+                        moduleName={module.name}
+                        moduleType={key as ModuleType}
+                        shellImage={module.image}
+                     />
                      <Typography variant='caption'>{module.name}</Typography>
                   </ListItemButton>
                ))}

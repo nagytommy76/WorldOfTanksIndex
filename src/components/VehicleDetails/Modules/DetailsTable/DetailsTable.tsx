4e3ca34202523c987ead273e23f0af1eabfad4f1
nagytommy76 <nagytommy76@gmail.com>
2025-09-16 19:39:04 +0200

    Add Mobility component

--- src/components/VehicleDetails/Modules/DetailsTable/DetailsTable.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/DetailsTable.tsx
@@ -5,13 +5,15 @@ import TableContainer from '@mui/material/TableContainer'
 import Paper from '@mui/material/Paper'
 
 import Firepower from './Includes/Firepower'
+import Mobility from './Mobility/Mobility'
 
 export default function DetailsTable() {
    return (
       <section className='w-full'>
          <Typography variant='h5'>Tank specifications</Typography>
-         <TableContainer className='w-1/2' component={Paper}>
+         <TableContainer className='w-2/5' component={Paper}>
             <Firepower />
+            <Mobility />
          </TableContainer>
       </section>
    )

59704ac55cf5ab6dd59d582119d9a534008418b7
nagytommy76 <nagytommy76@gmail.com>
2025-09-17 19:54:24 +0200

    Using Survivability & Other table component

--- src/components/VehicleDetails/Modules/DetailsTable/DetailsTable.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/DetailsTable.tsx
@@ -4,16 +4,20 @@ import Typography from '@mui/material/Typography'
 import TableContainer from '@mui/material/TableContainer'
 import Paper from '@mui/material/Paper'
 
-import Firepower from './Includes/Firepower'
+import Firepower from './Firepower/Firepower'
 import Mobility from './Mobility/Mobility'
+import Survivability from './Survivability/Survivability'
+import Other from './Other/Other'
 
 export default function DetailsTable() {
    return (
       <section className='w-full'>
          <Typography variant='h5'>Tank specifications</Typography>
-         <TableContainer className='w-2/5' component={Paper}>
+         <TableContainer component={Paper} className='min-h-1/2 grid grid-cols-2 grid-rows-2 gap-4'>
             <Firepower />
             <Mobility />
+            <Survivability />
+            <Other />
          </TableContainer>
       </section>
    )

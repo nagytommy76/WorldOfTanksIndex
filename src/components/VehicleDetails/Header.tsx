608c4c3422e8f0f03448c8288066aa9887584c37
nagytommy76 <nagytommy76@gmail.com>
2025-09-21 11:43:27 +0200

    Add styles
    Add mobile first style

--- src/components/VehicleDetails/Header.tsx
+++ src/components/VehicleDetails/Header.tsx
@@ -1,33 +1,26 @@
 import Image from 'next/image'
+import Typography from '@mui/material/Typography'
+
 import hangar from '@/Imageshangar-bg.webp'
 
-export default function Header({
-   tank_id,
-   tank_name,
-   description,
-}: {
-   tank_id: string
-   tank_name: string
-   description: string
-}) {
+export default function Header({ tank_name, description }: { tank_name: string; description: string }) {
    return (
-      <header className='min-h-[600px] relative'>
+      <header className='min-h-[260px] relative lg:min-h-[600px]'>
          <Image
-            className='absolute top-0 left-0 -z-9'
+            className='absolute bottom-0 lg:top-0 left-0 -z-9'
             src={`https://eu-wotp.wgcdn.co/dcont/tankopedia_images/${tank_name.toLocaleLowerCase()}/${tank_name.toLocaleLowerCase()}_image.png`}
             alt={tank_name}
             width={1500}
             height={900}
          />
          <Image
-            className='absolute top-0 left-0 object-cover -z-10'
+            className='absolute bottom-0 lg:top-0 left-0 object-cover -z-10'
             src={hangar.src}
             alt={tank_name}
             width={1920}
             height={900}
          />
-         <h1>{description}</h1>
-         <h1>{tank_id}</h1>
+         <Typography variant='body2'>{description}</Typography>
       </header>
    )
 }

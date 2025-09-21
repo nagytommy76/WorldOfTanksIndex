49b2bb4798e695838ac7e08b08f43f7aa3b7ba80
nagytommy76 <nagytommy76@gmail.com>
2025-09-21 11:44:10 +0200

    Add useSuspenseQuery
    Removed useEffect & state

--- src/components/VehicleDetails/Context/Hooks/useGetTomatoTankStats.tsx
+++ src/components/VehicleDetails/Context/Hooks/useGetTomatoTankStats.tsx
@@ -1,28 +1,20 @@
 import axios from '@/ProvidersAxiosProvider'
-import { useQuery } from '@tanstack/react-query'
-
-import { useState, useEffect } from 'react'
+import { useSuspenseQuery } from '@tanstack/react-query'
 
 import type { ITomatoTankStats } from '@/types/VehicleDetails/tomatoGGTankStats'
-
 // https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tanks/9745/e-75/EU.json?tankId=9745&name=e-75&server=EU
 // https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tank-stats.json
 
 export default function useGetTomatoTankStats(tank_short_name: string, tank_id: string) {
-   const [tomatoTankStats, setTomatoTankStats] = useState<ITomatoTankStats>({} as ITomatoTankStats)
    async function getTomatoTankStats() {
       const response = (await axios.get(`/${tank_id}/${tank_short_name}`)) as { data: ITomatoTankStats }
       return response.data
    }
 
-   const { data, isError } = useQuery({
+   const { data, isLoading, isError } = useSuspenseQuery({
       queryKey: ['tomato-tank-stats', tank_short_name],
       queryFn: getTomatoTankStats,
    })
 
-   useEffect(() => {
-      if (!isError && data) setTomatoTankStats(data)
-   }, [data, isError])
-
-   return tomatoTankStats
+   return { data, isLoading, isError }
 }

72abc1bd7148ec837de610813158b810b47ca943
nagytommy76 <nagytommy76@gmail.com>
2025-11-27 20:34:12 +0100

    Add dbConnect function

---
+++ src/lib/ConnectDB.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+export default async function dbConnect() {
+   const MONGODB_URI = process.env.NEXT_PUBLIC_MONGO_CONNECTION_STRING!
+
+   try {
+      if (!MONGODB_URI) {
+         throw new Error('Please define the MONGODB_URI environment variable')
+      }
+      const { connection } = await mongoose.connect(MONGODB_URI as string)
+      if (connection.readyState === 1) {
+         return Promise.resolve(connection)
+      }
+   } catch (error) {
+      console.error(error)
+      return Promise.reject(error)
+   }
+}

1d3c2859bb0de6ae9e9f133f53f2d363a6a56793
nagytommy76 <nagytommy76@gmail.com>
2025-11-27 20:34:43 +0100

    Get vehicle stats from mongoDB

--- app/api/[tank_id]/[tank_name]/route.ts
+++ app/api/[tank_id]/[tank_name]/route.ts
@@ -1,31 +1,20 @@
 import { NextResponse } from 'next/server'
+import dbConnect from '@/lib/ConnectDB'
+import { VehicleModel } from '@Models/TankModel'
 
 export async function GET(
    req: Request,
    { params }: { params: Promise<{ tank_id: string; tank_name: string }> }
 ) {
    const { tank_id, tank_name } = await params
-   // const url = `https://tomato.gg/_next/data/MibomUS6JqK7jWsXG9--J/en/tanks/${tank_id}/${tank_name}/EU.json`
-   // const url = `https://tomato.gg/_next/data/yxdJe2YiBXUm30Jk6B22f/en/tanks/${tank_id}/${tank_name}/EU.json`
+   await dbConnect()
    try {
-      const result = await fetch(`https://tomato.gg/_next/data/en/tanks/${tank_id}/${tank_name}/EU.json`)
-      const html = await result.text()
-      const match = html.match(/<script id="__NEXT_DATA__" type="application\/json">(.*?)<\/script>/)
+      const vehicleStats = await VehicleModel.findOne({
+         id: Number(tank_id),
+         // xmlId: tank_name,
+      })
 
-      if (match) {
-         const json = JSON.parse(match[1])
-         const res = await fetch(
-            `https://tomato.gg/_next/data/${json.buildId}/en/tanks/${tank_id}/${tank_name}/EU.json`,
-            {
-               headers: {
-                  'User-Agent': 'Mozilla/5.0', // optional, helps avoid being blocked
-               },
-            }
-         )
-         const data = await res.json()
-         return NextResponse.json(data.pageProps)
-      }
-      return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 })
+      return NextResponse.json({ vehicleStats }, { status: 200 })
    } catch (err) {
       return NextResponse.json({ error: err, details: (err as Error).message }, { status: 500 })
    }

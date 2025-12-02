e57a37731924a4311c51474dda5d06032c239595
nagytommy76 <nagytommy76@gmail.com>
2025-12-02 20:12:49 +0100

    Rename Dropdown -> FlagLinks

--- src/components/Navbar/includes/Menu/FlagLinks.tsx
+++ src/components/Navbar/includes/Menu/FlagLinks.tsx
@@ -6,12 +6,18 @@ import Link from 'next/link'
 
 import { flagSources } from '@/Base/FlagLinks/FlagLinks'
 
-export default function Dropdown({ handleClose }: { handleClose: () => void }) {
+export default function FlagLinks({
+   handleClose,
+   path = 'techtree',
+}: {
+   handleClose: () => void
+   path?: string
+}) {
    return (
       <>
          {Object.keys(flagSources).map((key) => {
             return (
-               <Link href={`/techtree/${key}`} key={key}>
+               <Link href={`/${path}/${key}`} key={key}>
                   <MenuItem onClick={handleClose} className={'w-[190px] flex flex-row gap-2'}>
                      <Image src={flagSources[key].source} alt={flagSources[key].alt} width={30} height={30} />{' '}
                      {flagSources[key].text}

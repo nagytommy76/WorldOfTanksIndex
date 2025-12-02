d7607149dd3395d50cebe4cab511df0ac6f43439
nagytommy76 <nagytommy76@gmail.com>
2025-12-02 20:11:39 +0100

    Add AccordionMenu component
    Using Accordion to show an expanded flagLinks link

---
+++ src/components/Navbar/includes/Menu/AccordionMenu.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+import FlagLinks from './FlagLinks'
+
+export default function AccordionMenu({
+   expanded,
+   handleExpandChange,
+   handleClose,
+   path = 'techtree',
+   accordionName = 'Tech Tree Tanks',
+}: {
+   expanded: string | false
+   handleExpandChange: (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => void
+   handleClose: () => void
+   path?: string
+   accordionName?: string
+}) {
+   return (
+      <Accordion
+         expanded={expanded === accordionName.toLowerCase()}
+         onChange={handleExpandChange(accordionName.toLowerCase())}
+      >
+         <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`${accordionName}-content`}
+            id={`${accordionName}-header`}
+         >
+            <Typography component='span' sx={{ width: '190px', flexShrink: 0 }}>
+               {accordionName}
+            </Typography>
+         </AccordionSummary>
+         <AccordionDetails>
+            <FlagLinks handleClose={handleClose} path={path} />
+         </AccordionDetails>
+      </Accordion>
+   )
+}

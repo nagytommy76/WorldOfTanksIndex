07ddda9bcb5bb0c59486d8c85eab662654c78f29
nagytommy76 <nagytommy76@gmail.com>
2025-12-03 20:06:23 +0100

    Add /vehicles

--- src/components/Techtree/Header/FlagLinks.tsx
+++ src/components/Techtree/Header/FlagLinks.tsx
@@ -7,13 +7,18 @@ import { flagSources } from '@/Base/FlagLinks/FlagLinks'
 
 export default function FlagLinks() {
    const pathname = usePathname()
+   const vehicleType = pathname && pathname.split('/')[2]
    return (
       <section className={'flex flex-row justify-center gap-5'}>
          {Object.keys(flagSources).map((key) => {
             return (
                <Link
-                  className={pathname === `/techtree/${key}` ? 'opacity-100' : 'opacity-50 hover:opacity-100'}
-                  href={`/techtree/${key}`}
+                  className={
+                     pathname === `/vehicles/${vehicleType}/${key}`
+                        ? 'opacity-100'
+                        : 'opacity-50 hover:opacity-100'
+                  }
+                  href={`/vehicles/${vehicleType}/${key}`}
                   key={key}
                >
                   <Image

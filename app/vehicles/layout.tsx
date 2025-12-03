ebb12d0a99ae4667c4834d20505c53e739831945
nagytommy76 <nagytommy76@gmail.com>
2025-12-03 20:01:44 +0100

    Moved layout and loading to /vehicles

--- app/vehicles/layout.tsx
+++ app/vehicles/layout.tsx
@@ -1,13 +1,9 @@
 import { Metadata } from 'next'
-
-import FlagLinks from '@/componentsTechtree/Header/FlagLinks'
 import HeaderTitle from '@/componentsTechtree/Header/HeaderTitle'
 
 export const metadata: Metadata = {
    keywords: [
       'world of tanks tech tree',
-      'world of tanks premium vehicles',
-      "world of tanks collector's vehicles",
       'world of tanks equipment',
       'world of tanks crews',
       'world of tanks crew',
@@ -24,7 +20,6 @@ export default async function layout({
    return (
       <section className={'min-h-screen'}>
          <HeaderTitle />
-         <FlagLinks />
          {children}
       </section>
    )

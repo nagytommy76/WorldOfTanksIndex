bcb74e7505755daff08ec7b4569c65ce09e1c57a
nagytommy76 <nagytommy76@gmail.com>
2025-12-09 19:53:31 +0100

    Fetch vehicle stats on the server

--- app/[tank_id]/[tank_name]/page.tsx
+++ app/[tank_id]/[tank_name]/page.tsx
@@ -1,38 +1,36 @@
-import Header from '@/VehicleDetails/Header'
-import Modules from '@/VehicleDetails/Modules/Modules'
-import { IModules } from '@/types/VehicleDetails/module'
+import type { ITankData } from '@/types/VehicleDetails/Vehicle'
 
-// https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=fefeeb22948e9ab6ed8c62a09515d476&tank_id=9745
-// https://api.worldoftanks.eu/wot/encyclopedia/vehicleprofile/?application_id=fefeeb22948e9ab6ed8c62a09515d476&tank_id=9745
+import Header from '@/VehicleDetails/Header'
+import VehicleContextProvider from '@/VehicleContext/VehicleContext'
+import ModuleSelect from '@/VehicleDetails/Modules/ModuleSelect/ModuleSelect'
+import DetailsTable from '@/VehicleDetails/Modules/DetailsTable/DetailsTable'
 
 // Big tank image: Vehicle tag: "tag": "G89_Leopard1" -> .toLocaleLowerCase() method needed
 // https://eu-wotp.wgcdn.co/dcont/tankopedia_images/g89_leopard1/g89_leopard1_image.png
 
-async function getModulesTree(tank_id: string | number) {
-   const filteredData = await fetch(
-      // `https://api.worldoftanks.eu/wot/encyclopedia/vehicles/
-      // ?application_id=${process.env.WOT_APP_ID}
-      // &fields=radios%2Csuspensions%2Cengines%2Ccrew%2Cguns%2Cdescription%2Cnext_tanks%2Cmodules_tree%2Cnation%2Ctier%2Cdefault_profile%2Cturrets&tank_id=${tank_id}`,
-      `https://api.worldoftanks.eu/wot/encyclopedia/vehicles/
-      ?application_id=${process.env.WOT_APP_ID}
-      &fields=description&tank_id=${tank_id}`,
-      { method: 'GET' }
-   )
-   const response = (await filteredData.json()) as Promise<{
-      data: { [tank_id: number]: { description: string; modules_tree: { [module_id: number]: IModules } } }
-   }>
+async function getTankDetails(tank_id: number, tank_name: string) {
+   const URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+   const vehicleStats = await fetch(`${URL}/api/${tank_id}/${tank_name}`, { method: 'GET' })
+   const response = (await vehicleStats.json()) as Promise<{ vehicleStats: ITankData }>
 
-   return await response
+   return (await response).vehicleStats
 }
 
 export default async function page({ params }: { params: Promise<{ tank_id: string; tank_name: string }> }) {
    const { tank_id, tank_name } = await params
-   const { data: modulesTree } = await getModulesTree(tank_id)
+   const tankStats = await getTankDetails(Number(tank_id), tank_name)
 
    return (
-      <section className='min-h-screen'>
-         <Header tank_name={tank_name} description={modulesTree[Number(tank_id)].description} />
-         <Modules tank_short_name={tank_name} tank_id={tank_id} />
+      <section className='min-h-screen max-w-screen-2xl mx-auto px-5 my-10'>
+         <Header tank_name={tank_name} description={tankStats.tankDetails?.description || ''} />
+         <VehicleContextProvider tankDetails={tankStats}>
+            <section
+               className={'flex w-full min-h-screen flex-col gap-0 xl:flex-row xl:gap-5 xl:justify-between'}
+            >
+               <ModuleSelect />
+               <DetailsTable />
+            </section>
+         </VehicleContextProvider>
       </section>
    )
 }

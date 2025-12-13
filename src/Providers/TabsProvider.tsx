de42a9e8ce77dc4c4b251fc4152b0517bb3cd221
nagytommy76 <nagytommy76@gmail.com>
2025-12-13 13:04:13 +0100

    Add TabsProvider
    Link between vehicle data: modules/stats/wn8 etc

---
+++ src/Providers/TabsProvider.tsx
@@ -0,0 +1,33 @@
+'use client'
+import Link from 'next/link'
+import { useState } from 'react'
+
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+
+interface LinkTabProps {
+   label?: string
+   href: string
+   selected?: boolean
+}
+
+function LinkTab(props: LinkTabProps) {
+   return <Tab component={Link as 'a' | typeof Link} aria-current={props.selected && 'page'} {...props} />
+}
+
+export default function TabsProvider({ baseHref }: { baseHref: string }) {
+   const [value, setValue] = useState(0)
+
+   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue)
+   }
+   return (
+      <section className='my-7'>
+         <Tabs value={value} onChange={handleChange} aria-label='nav tabs example' role='navigation'>
+            <LinkTab label='Modules' href={`${baseHref}/modules`} />
+            <LinkTab label='Mark of excellence' href={`${baseHref}/excellence`} />
+            <LinkTab label='WN8' href={`${baseHref}/`} />
+         </Tabs>
+      </section>
+   )
+}

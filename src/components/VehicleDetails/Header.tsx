8b910d64815d32c21774b3be3d22464d085b84d2
nagytommy76 <nagytommy76@gmail.com>
2025-12-13 13:07:01 +0100

    Remove description

--- src/components/VehicleDetails/Header.tsx
+++ src/components/VehicleDetails/Header.tsx
@@ -1,26 +1,23 @@
 import Image from 'next/image'
-import Typography from '@mui/material/Typography'
-
 import hangar from '@/Imageshangar-bg.webp'
 
-export default function Header({ tank_name, description }: { tank_name: string; description: string }) {
+export default function Header({ tank_name }: { tank_name: string }) {
    return (
-      <header className='min-h-[260px] relative lg:min-h-[600px]'>
+      <header className='min-h-[260px] relative lg:min-h-[750px] max-w-screen'>
          <Image
             className='absolute bottom-0 lg:top-0 left-0 -z-9'
             src={`https://eu-wotp.wgcdn.co/dcont/tankopedia_images/${tank_name.toLocaleLowerCase()}/${tank_name.toLocaleLowerCase()}_image.png`}
             alt={tank_name}
-            width={1500}
+            width={1920}
             height={900}
          />
          <Image
             className='absolute bottom-0 lg:top-0 left-0 object-cover -z-10'
             src={hangar.src}
-            alt={tank_name}
+            alt={'Hangar background'}
             width={1920}
             height={900}
          />
-         <Typography variant='body2'>{description}</Typography>
       </header>
    )
 }

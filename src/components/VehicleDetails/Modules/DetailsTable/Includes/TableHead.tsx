d7dc90a57e45cdc38c1da90a5cbf3e0b2574594b
nagytommy76 <nagytommy76@gmail.com>
2025-12-23 20:41:45 +0100

    Add iconSrc

--- src/components/VehicleDetails/Modules/DetailsTable/Includes/TableHead.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Includes/TableHead.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Typography from '@mui/material/Typography'
 import TableCell from '@mui/material/TableCell'
 import TableHead from '@mui/material/TableHead'
@@ -5,18 +6,22 @@ import TableRow from '@mui/material/TableRow'
 
 export default function TableHeadComponent({
    headTitle,
-   className = 'bg-lime-900',
+   className = 'bg-lime-950',
+   iconSrc,
 }: {
    headTitle: string
    className?: string
+   iconSrc?: string
 }) {
    return (
-      <TableHead>
-         <TableRow className={className}>
-            <TableCell>
+      <TableHead className={className}>
+         <TableRow>
+            <TableCell variant='head' className='border-b-0'>
                <Typography variant='h5'>{headTitle}</Typography>
             </TableCell>
-            <TableCell align='right'>icon</TableCell>
+            <TableCell variant='head' className='h-full flex justify-end border-b-0'>
+               {iconSrc && <Image src={iconSrc} alt='headTitle' width={30} height={30} title={headTitle} />}
+            </TableCell>
          </TableRow>
       </TableHead>
    )

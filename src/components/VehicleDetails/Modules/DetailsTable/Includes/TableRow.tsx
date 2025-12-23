9f8a240542a4f90c21226dc4f257f411bc25f53f
nagytommy76 <nagytommy76@gmail.com>
2025-12-23 20:41:16 +0100

    Add Image icon

--- src/components/VehicleDetails/Modules/DetailsTable/Includes/TableRow.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Includes/TableRow.tsx
@@ -1,31 +1,46 @@
 import TableCell from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
 import Typography from '@mui/material/Typography'
+import Image from 'next/image'
 
 export default function TableRowComponent({
    titleText,
    valueText,
    unit,
    paddingLeft,
+   iconSrc,
 }: {
    titleText: string
    valueText: string | number
    unit?: string
    paddingLeft?: boolean
+   iconSrc?: string
 }) {
    return (
-      <TableRow className='max-h-[25px]'>
-         <TableCell component='th' scope='row'>
-            <Typography className={paddingLeft ? 'pl-3' : ''} variant='caption'>
-               {titleText}
-            </Typography>
+      <TableRow className='max-h-[25px] ' hover>
+         <TableCell component='th' scope='row' className='px-1'>
+            <div className={`${paddingLeft ? 'pl-7' : ''} flex felx-row items-center gap-2`}>
+               {iconSrc && (
+                  <Image
+                     src={iconSrc}
+                     alt={titleText}
+                     title={titleText}
+                     width={35}
+                     height={35}
+                     className='object-cover w-[35px]'
+                  />
+               )}
+               <Typography variant='body2' fontSize={14} sx={{ opacity: 0.85 }}>
+                  {titleText}
+               </Typography>
+            </div>
          </TableCell>
-         <TableCell align='right'>
+         <TableCell align='right' className='px-1'>
             <div className='flex flex-row justify-end gap-1 items-center'>
-               <Typography className='text-green-300' variant='subtitle2'>
+               <Typography className='text-white font-semibold' variant='subtitle1' fontSize={15}>
                   {valueText}
                </Typography>
-               <Typography className='opacity-80' variant='caption' fontSize={10}>
+               <Typography className='opacity-65' variant='subtitle2' fontSize={11}>
                   {unit}
                </Typography>
             </div>

50af7577dae26a27fc86cbe2046c0e139a43d29b
nagytommy76 <nagytommy76@gmail.com>
2025-12-30 19:46:30 +0100

    Add html tooltip -> show +- 25% rng

--- src/components/VehicleDetails/Modules/DetailsTable/Includes/TableRow.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Includes/TableRow.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+
+import { styled } from '@mui/material/styles'
 import TableCell from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
 import Typography from '@mui/material/Typography'
-import Image from 'next/image'
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip'
+
+const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
+   <Tooltip {...props} classes={{ popper: className }} />
+))(({}) => ({
+   [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: '#3d3d49',
+      borderRadius: '4px',
+      padding: '15px',
+   },
+}))
 
 const IMAGE_SIZE = 32
 
@@ -11,42 +24,46 @@ export default function TableRowComponent({
    unit,
    paddingLeft,
    iconSrc,
+   TooltipTitle,
 }: {
    titleText: string
    valueText: string | number
    unit?: string
    paddingLeft?: boolean
    iconSrc?: string
+   TooltipTitle?: React.ReactNode
 }) {
    return (
-      <TableRow className='h-[20px] ' hover>
-         <TableCell component='th' scope='row' className='px-1'>
-            <div className={`${paddingLeft ? 'pl-7' : ''} flex felx-row items-center gap-2`}>
-               {iconSrc && (
-                  <Image
-                     src={iconSrc}
-                     alt={titleText}
-                     title={titleText}
-                     width={IMAGE_SIZE}
-                     height={IMAGE_SIZE}
-                     className={`object-cover w-[${IMAGE_SIZE}px]`}
-                  />
-               )}
-               <Typography variant='body2' fontSize={14} sx={{ opacity: 0.85 }}>
-                  {titleText}
-               </Typography>
-            </div>
-         </TableCell>
-         <TableCell align='right' className='px-1'>
-            <div className='flex flex-row justify-end gap-1 items-center'>
-               <Typography className='text-white font-semibold' variant='subtitle1' fontSize={15}>
-                  {valueText}
-               </Typography>
-               <Typography className='opacity-65' variant='subtitle2' fontSize={11}>
-                  {unit}
-               </Typography>
-            </div>
-         </TableCell>
-      </TableRow>
+      <HtmlTooltip title={TooltipTitle}>
+         <TableRow className='h-[20px] ' hover>
+            <TableCell component='th' scope='row' className='px-1'>
+               <div className={`${paddingLeft ? 'pl-7' : ''} flex felx-row items-center gap-2`}>
+                  {iconSrc && (
+                     <Image
+                        src={iconSrc}
+                        alt={titleText}
+                        title={titleText}
+                        width={IMAGE_SIZE}
+                        height={IMAGE_SIZE}
+                        className={`object-cover w-[${IMAGE_SIZE}px]`}
+                     />
+                  )}
+                  <Typography variant='body2' fontSize={14} sx={{ opacity: 0.85 }}>
+                     {titleText}
+                  </Typography>
+               </div>
+            </TableCell>
+            <TableCell align='right' className='px-1'>
+               <div className='flex flex-row justify-end gap-1 items-center'>
+                  <Typography className='text-white font-semibold' variant='subtitle1' fontSize={15}>
+                     {valueText}
+                  </Typography>
+                  <Typography className='opacity-65' variant='subtitle2' fontSize={11}>
+                     {unit}
+                  </Typography>
+               </div>
+            </TableCell>
+         </TableRow>
+      </HtmlTooltip>
    )
 }

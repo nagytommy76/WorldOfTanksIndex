89847b7d7f2b1b85fb3899a8e8986477dcf03e71
nagytommy76 <nagytommy76@gmail.com>
2025-12-31 19:54:43 +0100

    Create Penetration component

---
+++ src/components/VehicleDetails/Modules/DetailsTable/Firepower/Includes/Penetration.tsx
@@ -0,0 +1,31 @@
+import TableRowComponent from '../../Includes/TableRow'
+import Typography from '@mui/material/Typography'
+
+export default function Penetration({ piercingPower }: { piercingPower: number[] }) {
+   return (
+      <TableRowComponent
+         iconSrc='/icons/firepower/avgPiercingPower.png'
+         titleText='Penetration (at 50/500 m)'
+         valueText={`
+                  ${piercingPower[0]} /
+                  ${piercingPower[1]}
+               `}
+         unit='mm'
+         TooltipTitle={
+            <section className='min-w-[220px] min-h-[90px] flex flex-col gap-2'>
+               <Typography variant='body1'>Avarage Penetration +/- 25%</Typography>
+               <div>
+                  <Typography variant='body1'>At 50 meters</Typography>
+                  <Typography variant='body2'>Min: {piercingPower[0] * 0.75} pen</Typography>
+                  <Typography variant='body2'>Max: {piercingPower[0] * 1.25} pen</Typography>
+               </div>
+               <div>
+                  <Typography variant='body1'>At 500 meters</Typography>
+                  <Typography variant='body2'>Min: {piercingPower[1] * 0.75} pen</Typography>
+                  <Typography variant='body2'>Max: {piercingPower[1] * 1.25} pen</Typography>
+               </div>
+            </section>
+         }
+      />
+   )
+}

8792741485aacccd052e076c1d77ba10f0a4ce64
nagytommy76 <nagytommy76@gmail.com>
2026-01-09 19:54:10 +0100

    Separate dmp calculations

--- src/components/VehicleDetails/Modules/DetailsTable/Firepower/Includes/helper.ts
+++ src/components/VehicleDetails/Modules/DetailsTable/Firepower/Includes/helper.ts
@@ -1,32 +1,34 @@
-import type { IClip } from '@/types/VehicleDetails/Guns'
+import type { IClip, IAutoReload } from '@/types/VehicleDetails/Guns'
 
-export default function returnDPM(
-   reloadTime: number,
-   damage: number,
-   clip?: null | IClip,
-   reloadBetweenShells?: number,
-   clipDamage?: number,
-   autoReaload?: number[]
-) {
-   if (autoReaload && clipDamage && reloadBetweenShells) {
-      let totalReloadTime = 0
-      autoReaload.reverse().map((time) => {
-         totalReloadTime += time
-      })
-
-      const dpm = (60 / (totalReloadTime - reloadBetweenShells)) * clipDamage
-      // console.log('TIME: ', (60 / (totalReloadTime - 5.61)) * clipDamage)
-      return dpm
-   }
-
-   if (clip && reloadBetweenShells && clipDamage) {
-      /**
-       * clip.count - 1 -> the first shell is in the magazine already
-       */
-      const totalReloadTime = (clip.count - 1) * reloadBetweenShells + reloadTime
-      const dpm = (60 / totalReloadTime) * clipDamage
-      return dpm
-   }
+export function returnDPM(reloadTime: number, damage: number) {
    const DPM = (60 / reloadTime) * damage
    return DPM
+}
+
+export function returnAutoReloadDPM(
+   autoReaload: IAutoReload,
+   clipDamage: number,
+   reloadBetweenShells: number
+) {
+   let totalReloadTime = 0
+   autoReaload.reloadTime.reverse().map((time) => {
+      totalReloadTime += time
+   })
+   const dpm = (60 / (totalReloadTime - reloadBetweenShells)) * clipDamage
+   // console.log('TIME: ', (60 / (totalReloadTime - 5.61)) * clipDamage)
+   return dpm
+}
+
+export function returnClipReloadTime(
+   reloadTime: number,
+   clip: IClip,
+   clipDamage: number,
+   reloadBetweenShells: number
+) {
+   /**
+    * clip.count - 1 -> the first shell is in the magazine already
+    */
+   const totalReloadTime = (clip.count - 1) * reloadBetweenShells + reloadTime
+   const dpm = (60 / totalReloadTime) * clipDamage
+   return dpm
 }

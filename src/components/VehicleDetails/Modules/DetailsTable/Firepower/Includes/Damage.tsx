5c15b52c58ff0c40546f8865a91b231e0b678858
nagytommy76 <nagytommy76@gmail.com>
2026-01-10 11:31:02 +0100

    Check if damage is array

--- src/components/VehicleDetails/Modules/DetailsTable/Firepower/Includes/Damage.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Firepower/Includes/Damage.tsx
@@ -1,7 +1,30 @@
 import TableRowComponent from '../../Includes/TableRow'
 import Typography from '@mui/material/Typography'
 
-export default function Damage({ damage }: { damage: number }) {
+export default function Damage({ damage }: { damage: number | number[] }) {
+   if (Array.isArray(damage)) {
+      return (
+         <TableRowComponent
+            iconSrc='/icons/firepower/avgDamage.png'
+            titleText='Damage'
+            valueText={`${damage[0]} / ${damage[1]}`}
+            unit='HP'
+            TooltipTitle={
+               <section className='w-[250px] h-[110px] flex flex-col gap-2'>
+                  <Typography variant='body1'>Avarage Damage +/- 25%</Typography>
+                  <Typography variant='body2'>50m / 500m</Typography>
+                  <Typography variant='body2'>
+                     Min: {damage[0] * 0.75} / {damage[1] * 0.75} dmg
+                  </Typography>
+                  <Typography variant='body2'>
+                     Max: {damage[0] * 1.25} / {damage[1] * 1.25} dmg
+                  </Typography>
+               </section>
+            }
+         />
+      )
+   }
+
    return (
       <TableRowComponent
          iconSrc='/icons/firepower/avgDamage.png'

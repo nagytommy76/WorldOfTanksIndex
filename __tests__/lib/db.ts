f75d1c291bb1d2ca35a98c85cfab25630a776b37
nagytommy76 <nagytommy76@gmail.com>
2026-01-27 20:17:48 +0100

    Create a mongodb-memory-server connect,stop and clear functions

---
+++ __tests__/lib/db.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+let mongoServer: MongoMemoryServer
+
+export async function startMongoMemoryServer() {
+   if (mongoose.connection.readyState === 0) {
+      mongoServer = await MongoMemoryServer.create()
+      await mongoose.connect(mongoServer.getUri())
+      console.log('Connected to in-memory database')
+   }
+   return mongoServer
+}
+
+export async function stopMongoMemoryServer() {
+   await mongoose.disconnect()
+   await mongoose.connection.close()
+   await mongoServer.stop()
+   console.log('MongoDB has stopped (stopMongoMemoryServer)')
+}
+
+export async function clearDatabase() {
+   const collections = mongoose.connection.collections
+   for (const key in collections) {
+      const collection = collections[key]
+      await collection.deleteMany({})
+   }
+}

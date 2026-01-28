f6d8f97912e4bf984f0d897d572085d0507cc821
nagytommy76 <nagytommy76@gmail.com>
2026-01-28 20:04:34 +0100

    Add Details Table test
    Working on the table query

---
+++ __tests__/DetailsTable.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen, within } from '@testing-library/react'
+import { useParams } from 'next/navigation'
+
+import ModuleSelect from '@/VehicleDetails/Modules/ModuleSelect/ModuleSelect'
+import DetailsTable from '@/VehicleDetails/Modules/DetailsTable/DetailsTable'
+import VehicleContextProvider from '@/VehicleContext/VehicleContext'
+
+import MockHirschkafer from './mocks/Hirschkafer'
+
+jest.mock('next/navigation', () => ({
+   useParams: jest.fn(),
+}))
+
+beforeAll(() => {
+   jest
+      .mocked(useParams)
+      .mockReturnValue({ tank_name: MockHirschkafer.xmlId, tank_id: MockHirschkafer._id.toString() })
+
+   render(
+      <VehicleContextProvider tankDetails={MockHirschkafer}>
+         <ModuleSelect />
+         <DetailsTable />
+      </VehicleContextProvider>,
+   )
+})
+
+function checkRowContents(row: HTMLElement, name: string) {
+   const columns = within(row).getAllByRole('cell')
+   //    expect(columns).toHaveLength(3)
+   expect(columns[0]).toHaveTextContent(name)
+}
+
+describe('Testing DetailsTable component', () => {
+   it('should render Hirschkafer Mechanic name', () => {
+      expect(screen.getByText(/Propellant Thermal Control System/i)).toBeInTheDocument()
+   })
+   it('should render Hirschkafer Firepower tabel elements', async () => {
+      //   const rows = screen.getAllByRole('cell')
+      //   console.log(rows)
+      //   expect(screen.getAllByRole('row'))
+      const tables = await screen.findAllByRole('table')
+
+      console.log(tables[0].outerText)
+      // first rowgroup is for the thead second is for tbody
+      //   const tbody = within(table).getAllByRole('rowgroup')[1]
+      //   const rows = within(tbody).getAllByRole('row')
+
+      //   checkRowContents(rows[0], '600HP')
+   })
+})

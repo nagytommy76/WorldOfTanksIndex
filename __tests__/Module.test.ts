b3f51c57a46fd6dfbb12a57b1ede6a31d5a85061
nagytommy76 <nagytommy76@gmail.com>
2026-01-28 20:03:41 +0100

    Add jest test environment: node -> test API call

--- __tests__/Module.test.ts
+++ __tests__/Module.test.ts
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment node
+ */
 import { NextRequest } from 'next/server'
 import { VehicleModel } from '@Models/TankModel'
 import { GET } from '@/app/api/[tank_id]/[tank_name]/route'

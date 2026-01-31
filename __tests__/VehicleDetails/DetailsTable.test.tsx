1326ab92a587676d646417c6c0c488154556c4ce
nagytommy76 <nagytommy76@gmail.com>
2026-01-31 10:08:12 +0100

    Move DetailsTable test to VehicleDetails folder

--- __tests__/VehicleDetails/DetailsTable.test.tsx
+++ __tests__/VehicleDetails/DetailsTable.test.tsx
@@ -5,7 +5,7 @@ import ModuleSelect from '@/VehicleDetails/Modules/ModuleSelect/ModuleSelect'
 import DetailsTable from '@/VehicleDetails/Modules/DetailsTable/DetailsTable'
 import VehicleContextProvider from '@/VehicleContext/VehicleContext'
 
-import MockHirschkafer from './mocks/Hirschkafer'
+import MockHirschkafer from '../mocks/Hirschkafer'
 
 jest.mock('next/navigation', () => ({
    useParams: jest.fn(),
@@ -24,31 +24,20 @@ beforeEach(() => {
    )
 })
 
-function checkRowContents(row: HTMLElement, name: string) {
-   const columns = within(row).getAllByRole('cell')
-   //    expect(columns).toHaveLength(3)
-   expect(columns[0]).toHaveTextContent(name)
-}
-
 describe('Testing DetailsTable component', () => {
    it('should render Hirschkafer Mechanic name', () => {
       expect(screen.getByText(/Propellant Thermal Control System/i)).toBeInTheDocument()
    })
    it('should render Hirschkafer Firepower tabel elements', async () => {
-      //   const rows = screen.getAllByRole('cell')
-      //   console.log(rows)
-      //   expect(screen.getAllByRole('row'))
-      // const tables = screen.getByLabelText(/Firepower table with average damage and penetration/i)
-
       const firepowerTable = screen.getByRole('table', {
          name: /firepower/i,
       })
 
-      // console.log(firepowerTable)
-      // first rowgroup is for the thead second is for tbody
-      //   const tbody = within(table).getAllByRole('rowgroup')[1]
-      //   const rows = within(tbody).getAllByRole('row')
+      // const tbody = within(firepowerTable).getAllByRole('rowgroup')
+      // const rows = within(tbody[1]).getAllByRole('row')
+      // console.log(rows[0].innerHTML)
 
-      //   checkRowContents(rows[0], '600HP')
+      expect(within(firepowerTable).getByText('Damage')).toBeInTheDocument()
+      // expect(within(firepowerTable).getByText(/600HP/i)).toBeInTheDocument()
    })
 })

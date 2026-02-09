eaebc3e40bd279cbbe42c8c19d6f4e69bb473d5d
nagytommy76 <nagytommy76@gmail.com>
2026-02-09 19:20:44 +0100

    Add styles & flagSize/opacity props

--- src/components/VehiclesTable/Includes/FlagLinks.tsx
+++ src/components/VehiclesTable/Includes/FlagLinks.tsx
@@ -5,28 +5,41 @@ import { usePathname } from 'next/navigation'
 
 import { flagSources } from '@/Base/FlagLinks/FlagLinks'
 
-export default function FlagLinks() {
+export default function FlagLinks({
+   vehicleTypeProp = null,
+   flagSize = 70,
+   opacity = 70,
+}: {
+   vehicleTypeProp?: string | null
+   flagSize?: number
+   opacity?: number
+}) {
    const pathname = usePathname()
    const vehicleType = pathname && pathname.split('/')[2]
    return (
-      <section className={'flex flex-row justify-center gap-5'}>
+      <section
+         className={
+            'grid grid-cols-3 gap-2 justify-items-center xl:flex xl:flex-row xl:justify-center xl:gap-5'
+         }
+      >
          {Object.keys(flagSources).map((key) => {
             return (
                <Link
                   className={
-                     pathname === `/vehicles/${vehicleType}/${key}`
+                     pathname ===
+                     `/vehicles/${vehicleType === undefined ? vehicleTypeProp : vehicleType}/${key}`
                         ? 'opacity-100'
-                        : 'opacity-50 hover:opacity-100'
+                        : `opacity-${opacity} hover:opacity-100 transition-all duration-150`
                   }
-                  href={`/vehicles/${vehicleType}/${key}`}
+                  href={`/vehicles/${vehicleType === undefined ? vehicleTypeProp : vehicleType}/${key}`}
                   key={key}
                >
                   <Image
                      src={flagSources[key].source}
                      alt={flagSources[key].alt}
                      title={flagSources[key].alt}
-                     width={70}
-                     height={70}
+                     width={flagSize}
+                     height={flagSize}
                   />
                </Link>
             )

5b042f9baf3913c2421be2f6c4ab2b16b5b29967
nagytommy76 <nagytommy76@gmail.com>
2026-02-10 19:58:00 +0100

    Copy PriceCell component to TankCard

---
+++ src/components/Base/TankCard/PriceCell.tsx
@@ -0,0 +1,40 @@
+import Typography from '@mui/material/Typography'
+import Image from 'next/image'
+
+function Price({
+   vehiclePrice,
+   isGold = false,
+}: {
+   vehiclePrice: number | { gold: number | null }
+   isGold?: boolean
+}) {
+   return (
+      <div className='flex flex-row items-center gap-1'>
+         <Image
+            src={isGold ? '/icons/money_gold.webp' : '/icons/money_silver.webp'}
+            alt={isGold ? 'Gold' : 'Credits'}
+            width={25}
+            height={25}
+            className='object-cover w-[30px]'
+         />
+         <Typography variant='subtitle2' color={isGold ? 'primary' : ''} className='text-xs'>
+            {isGold
+               ? (vehiclePrice as { gold: number }).gold.toLocaleString()
+               : (vehiclePrice as number).toLocaleString()}{' '}
+         </Typography>
+      </div>
+   )
+}
+
+export default function PriceCell({ vehiclePrice }: { vehiclePrice: number | { gold: number | null } }) {
+   if (vehiclePrice === null) return null
+   return (
+      <>
+         {typeof vehiclePrice === 'number' ? (
+            <Price vehiclePrice={vehiclePrice} />
+         ) : (
+            vehiclePrice.gold !== null && <Price vehiclePrice={vehiclePrice} isGold />
+         )}
+      </>
+   )
+}

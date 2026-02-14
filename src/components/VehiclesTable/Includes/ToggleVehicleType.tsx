ac6f17e96c47a50b2b5c9e071732cbd91982d403
nagytommy76 <nagytommy76@gmail.com>
2026-02-14 20:00:13 +0100

    Add Toggle vehicle type button

---
+++ src/components/VehiclesTable/Includes/ToggleVehicleType.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import { useContext } from 'react'
+import { OrderContext } from '@/OrderContext/OrderContext'
+
+import getIcon, { vehicleTypes } from '@/lib/getVehicleTypeIcon'
+import retrunVehicleType from '@/helpers/returnVehicleType'
+
+import Tooltip from '@mui/material/Tooltip'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+
+export default function ToggleVehicleType() {
+   const {
+      orderDispatch,
+      orderReducer: { vehicleTypesToggle },
+   } = useContext(OrderContext)
+
+   return (
+      <ToggleButtonGroup
+         value={vehicleTypesToggle}
+         onChange={(_, value: string[]) => {
+            orderDispatch({ type: 'SET_TYPE_TOGGLE', payload: value })
+         }}
+         aria-label='text alignment'
+      >
+         {vehicleTypes.map((type) => {
+            return (
+               <Tooltip key={type} title={retrunVehicleType(type)} placement='top' arrow>
+                  <ToggleButton value={type} aria-label={type}>
+                     <Image src={getIcon(type)} alt={type} width={16} height={16} />
+                  </ToggleButton>
+               </Tooltip>
+            )
+         })}
+      </ToggleButtonGroup>
+   )
+}

5c5625ffd4473fc27c3a7fe73708242d7910f9ff
nagytommy76 <nagytommy76@gmail.com>
2026-02-14 20:00:40 +0100

    Add Toggle vehicle tier button

---
+++ src/components/VehiclesTable/Includes/ToogleTier.tsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import { OrderContext } from '@/OrderContext/OrderContext'
+
+import Typography from '@mui/material/Typography'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+
+import tiers from '@/lib/tierList'
+
+export default function ToogleTier() {
+   const {
+      orderDispatch,
+      orderReducer: { vehicleTierToggle },
+   } = useContext(OrderContext)
+   return (
+      <ToggleButtonGroup
+         value={vehicleTierToggle}
+         onChange={(_, value: number[]) => {
+            orderDispatch({ type: 'SET_TIER_TOGGLE', payload: value })
+         }}
+         aria-label='text alignment'
+      >
+         {tiers.map((tier, index) => {
+            return (
+               <ToggleButton key={index} value={index + 1} aria-label='Vehicle tier'>
+                  <Typography variant='caption'>{tier}</Typography>
+               </ToggleButton>
+            )
+         })}
+      </ToggleButtonGroup>
+   )
+}

c5c91fceb2349197e1caa4df256bf5f440cb6963
nagytommy76 <nagytommy76@gmail.com>
2026-02-22 11:53:10 +0100

    Reset shell modifiers = null

--- src/components/VehicleDetails/Context/ModifiersReducer.tsx
+++ src/components/VehicleDetails/Context/ModifiersReducer.tsx
@@ -1,4 +1,4 @@
-import type { IModifiersReducerState, IModifiersContextActions } from './ModifiersTypes'
+import { type IModifiersReducerState, type IModifiersContextActions } from './ModifiersTypes'
 
 export default function ModifiersReducer(
    state: IModifiersReducerState,
@@ -8,7 +8,25 @@ export default function ModifiersReducer(
       case 'SET_SHELLS_MODIFIERS':
          return { ...state, modifiers: { ...state.modifiers, shells: payload } }
       case 'RESET_SHELLS_MODIFIERS':
-         return { ...state, modifiers: { ...state.modifiers, shells: {} } }
+         return {
+            ...state,
+            modifiers: {
+               ...state.modifiers,
+               shells: {
+                  'damage.armor': null,
+                  speed: null,
+                  maxDistance: null,
+                  price: null,
+                  'piercingPower[0]': null,
+                  'piercingPower[1]': null,
+               },
+            },
+         }
+      case 'SET_DEFAULT_SHELL_NAME':
+         return {
+            ...state,
+            defaultModuleNames: { ...state.defaultModuleNames, shells: payload.shells },
+         }
       default:
          return state
    }

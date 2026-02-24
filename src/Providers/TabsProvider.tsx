af07462e3ed995fb7fe5e5bfc03c99f49f7ca5ce
nagytommy76 <nagytommy76@gmail.com>
2026-02-24 19:52:00 +0100

    Add styles to link tab

--- src/Providers/TabsProvider.tsx
+++ src/Providers/TabsProvider.tsx
@@ -6,14 +6,36 @@ import { useState } from 'react'
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 
+import { styled } from '@mui/material/styles'
+
+const LinkTab = styled((props: LinkTabProps) => (
+   <Tab component={Link as 'a' | typeof Link} aria-current={props.selected && 'page'} {...props} />
+))(({ theme }) => ({
+   minWidth: 0,
+   [theme.breakpoints.up('sm')]: {
+      minWidth: 0,
+   },
+   fontWeight: theme.typography.fontWeightBold,
+   marginRight: 6,
+   color: 'rgb(236, 236, 236)',
+   backgroundColor: 'rgba(126, 125, 125, 0.329)',
+   borderRadius: 3,
+   '&:hover': {
+      color: theme.palette.primary.main,
+      opacity: 1,
+   },
+   '&.Mui-selected': {
+      color: theme.palette.primary.main,
+   },
+   '&.Mui-focusVisible': {
+      backgroundColor: '#924519',
+   },
+}))
+
 interface LinkTabProps {
    label?: string
    href: string
    selected?: boolean
-}
-
-function LinkTab(props: LinkTabProps) {
-   return <Tab component={Link as 'a' | typeof Link} aria-current={props.selected && 'page'} {...props} />
 }
 
 const PATHS: {
@@ -25,7 +47,7 @@ const PATHS: {
    modules: { displayText: 'Modules', index: 0 },
    gunmarks: { displayText: 'Marks of excellence', index: 1 },
    mastery: { displayText: 'Mastery Badge', index: 2 },
-   wn8: { displayText: 'WN8', index: 3 },
+   // wn8: { displayText: 'WN8', index: 3 },
 }
 
 export default function TabsProvider({ baseHref }: { baseHref: string }) {
@@ -42,7 +64,7 @@ export default function TabsProvider({ baseHref }: { baseHref: string }) {
          <Tabs
             value={value}
             onChange={handleChange}
-            aria-label='nav tabs example'
+            aria-label='Vehicle tabs'
             role='navigation'
             variant='scrollable'
             scrollButtons='auto'

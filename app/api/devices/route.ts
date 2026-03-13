241ca04865e09b38de25862eb76d02fc693c3e53
nagytommy76 <nagytommy76@gmail.com>
2026-03-13 19:56:11 +0100

    Using aggregate to query devices by provisions array

--- app/api/devices/route.ts
+++ app/api/devices/route.ts
@@ -1,15 +1,29 @@
 import dbConnect from '@/lib/ConnectDB'
+import { NextResponse, NextRequest } from 'next/server'
 
 import { DeviceModel } from '@Models/DevicesModel'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+   const provisions = req.nextUrl.searchParams.get('provisions')
+   if (!provisions) return NextResponse.json({ error: 'Provisions parameter is required' }, { status: 404 })
    try {
+      const provisionsArray = JSON.parse(provisions)
       await dbConnect()
 
-      const allDevices = await DeviceModel.find({}).lean()
+      const compatibleDevices = await DeviceModel.aggregate([
+         {
+            $match: {
+               id: { $in: provisionsArray },
+               deviceType: {
+                  $in: ['deluxe', 'tiers', 'trophy', 'modernized'],
+               },
+            },
+         },
+      ])
 
-      return Response.json({ allDevices }, { status: 200 })
+      return NextResponse.json({ compatibleDevices }, { status: 200 })
    } catch (error) {
       console.log(`Error during get devices: `, error)
+      return NextResponse.json({ error: 'Error during get devices', errorType: error }, { status: 500 })
    }
 }

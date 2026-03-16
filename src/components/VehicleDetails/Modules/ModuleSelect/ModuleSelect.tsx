26973f19cced4bc1e49320113ad8a665122a8b26
nagytommy76 <nagytommy76@gmail.com>
2026-03-16 20:01:12 +0100

    Using Devices component

--- src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
@@ -2,12 +2,14 @@
 
 import Modules from './Components/Modules'
 import Shells from './Components/Shells'
+import Devices from './Components/Devices'
 
 export default function ModuleSelect() {
    return (
       <aside className={'w-full flex flex-col items-center gap-4 py-5 xl:py-0 xl:w-[300px]'}>
          <Modules />
          <Shells />
+         <Devices />
       </aside>
    )
 }

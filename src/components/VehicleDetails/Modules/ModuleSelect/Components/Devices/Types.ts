76f20ecc076cc35ac0b5ddd197e2882a0750136a
nagytommy76 <nagytommy76@gmail.com>
2026-03-18 19:59:12 +0100

    Create OverlayTypes

---
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/Devices/Types.ts
@@ -0,0 +1,9 @@
+export type OverlayTypes =
+   | 'equipmentTrophyBasic'
+   | 'equipmentTrophyUpgraded'
+   | 'equipmentPlus'
+   | 'equipmentModernized_1'
+   | 'equipmentModernized_2'
+   | 'equipmentModernized_3'
+   | 'none'
+   | 'tiers'

f1eac3835200a6c77e02e48d5988d48c09128409
nagytommy76 <nagytommy76@gmail.com>
2026-03-25 20:05:13 +0100

    Add Device modifier descriptions

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/Devices/DeviceGroup/Includes/TooltipTitle.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/Devices/DeviceGroup/Includes/TooltipTitle.tsx
@@ -15,37 +15,35 @@ export default function TooltipTitle({
 }) {
    const { vehicleType } = useContext(VehicleContext)
    return (
-      <>
+      <section className='min-w-[260px] min-h-[100px] p-1'>
          {children}
-         <div className='min-w-[250px] min-h-[100px]'>
-            <h1>AGGREGATE M?ODIFIERS:::::</h1>
+         <div>
             {aggregateModifiers && (
                <div>
                   {aggregateModifiers.map((aggregateModifier, index) => (
                      <div key={index}>
                         {aggregateModifier.vehicleTypes.includes(vehicleType) && (
-                           <>
-                              <Typography>Vehicle Type: {vehicleType}</Typography>
+                           <Typography variant='body2'>
                               {ReturnModifierDisplayString(aggregateModifier.value)[aggregateModifier.name]}
-                              {/* {aggregateModifier.name}: {aggregateModifier.value} */}
-                           </>
+                           </Typography>
                         )}
                      </div>
                   ))}
                </div>
             )}
-            <h1>M?ODIFIERS:::::</h1>
             {modifiers && (
                <div>
                   {modifiers.map((modifier, index) => (
                      <div key={index}>
-                        {modifier.name}: {modifier.value}
+                        <Typography variant='body2'>
+                           {ReturnModifierDisplayString(modifier.value)[modifier.name]}
+                        </Typography>
                      </div>
                   ))}
                </div>
             )}
          </div>
-      </>
+      </section>
    )
 }
 type ModifierDisplayString = {
@@ -53,9 +51,88 @@ type ModifierDisplayString = {
 }
 
 function ReturnModifierDisplayString(value: number): ModifierDisplayString {
-   const transformValue = ((value - 1) * 100).toFixed(1)
-   console.log(transformValue)
+   const percentValue = (value - 1) * 100
+   const transformValue = Math.round(percentValue * 100) / 100
    return {
       vehicleCamouflage: `+${transformValue}% to concealment`,
+      vehicleStillCamouflage: `+${transformValue}% to concealment while stationary`,
+      vehicleStillCamouflageDeluxe: `+${transformValue}% to concealment while stationary`,
+      /**
+       * @description Ventillation
+       */
+      crewLevel: `+${transformValue}% to crew level`,
+      /**
+       * Modifiers - Gun Rammer
+       */
+      vehicleGunReloadTime: `${transformValue}% to reload time`,
+      /**
+       * @description Spall liner
+       */
+      vehicleHEShellDamageResistance: `${transformValue}% to HE shell damage`,
+      vehicleRamDamageResistance: `${transformValue}% to ramming damage`,
+      crewHitChance: `+${transformValue}% to crew protection from injuries`,
+      crewStunDuration: `${transformValue}% stun duration`,
+      crewRepeatedStunDuration: `${transformValue}% additional stun duration if already stunned`,
+      /**
+       * @description Optics
+       */
+      vehicleCircularVisionRadius: `+${transformValue}% to view range`,
+      /**
+       * @description Binoculars
+       */
+      vehicleStillCircularVisionRadius: `+${transformValue}% to view range while stationary`,
+      vehicleStillCircularVisionRadiusDeluxe: `+${transformValue}% to view range while stationary`,
+      /**
+       * @description CVS
+       */
+      demaskFoliageFactor: `${transformValue}% to concealment of enemy vehicles behind foliage`,
+      demaskMovingFactor: `${transformValue}% to concealment of moving enemy vehicles`,
+      /**
+       * @description Improved Radio Set
+       */
+      vehicleEnemySpottingTime: `+${transformValue}s duration an enemy vehicle is visible`,
+      vehicleOwnSpottingTime: `${transformValue}s duration you are visible to enemy vehicles`,
+      /**
+       * @description Gun laying drive
+       */
+      vehicleGunAimSpeed: `+${transformValue}% to aiming speed`,
+      /**
+       * @description Vertical Stabilizer
+       */
+      vehicleGunShotDispersion: `${transformValue}% to dispersion during movement and traverse`,
+      /**
+       * @description Hardening (HP boost)
+       */
+      vehicleStrength: `+${transformValue}% to hit points`,
+      vehicleChassisStrength: `+${transformValue}% to suspension durability`,
+      vehicleChassisRepairSpeed: `+${transformValue}% to suspension repair speed`,
+      vehicleChassisFallDamage: `${transformValue}% to collision damage`,
+      /**
+       * @description Additional Grousers
+       */
+      vehicleAllGroundRotationSpeed: `+${transformValue}% to vehicle traverse speed`,
+      vehicleSpeedGain: `+${transformValue}% to maintaining speed when moving across any terrain type`,
+      /**
+       * @description Modified Configuration
+       */
+      vehicleRepairSpeed: `+${transformValue}% to repair speed`,
+      vehicleAmmoBayEngineFuelStrength: `+${transformValue}% to ammo rack, fuel rank, and engine durability`,
+      vehPenaltyForDamagedEngine: `${transformValue}% to engine power penalty when damaged`,
+      vehPenaltyForDamagedAmmorack: `${transformValue}% to ammo rack durability penalty when damaged`,
+      vehicleFireChance: `${transformValue}% chance of engine fire`,
+      /**
+       * @description Improved Rotation Mechanism
+       */
+      vehicleTurretOrCuttingRotationSpeed: `+${transformValue}% to hull traverse speed`,
+      /**
+       * @description Improved Aiming
+       */
+      vehicleGunShotFullDispersion: `${transformValue}% to aiming circle size`,
+      /**
+       * @description Turbocharger
+       */
+      vehicleEnginePower: `+${transformValue}% to engine power`,
+      vehicleForwardMaxSpeed: `+${transformValue}% to top forward speed`,
+      vehicleBackwardMaxSpeed: `+${transformValue}% to top backward speed`,
    }
 }

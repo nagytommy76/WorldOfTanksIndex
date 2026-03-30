f21102eba8f2a06ae8b4dac81e42fed938fa9341
nagytommy76 <nagytommy76@gmail.com>
2026-03-30 20:13:52 +0200

    Add supplySlotActive case
    Show supplyCategory icon

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/Devices/DeviceGroup/Includes/MenuItemOverlay.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/Devices/DeviceGroup/Includes/MenuItemOverlay.tsx
@@ -7,6 +7,7 @@ import type { OverlayTypes } from '../../Types'
  * @param {OverlayTypes} overlayType Determines which overlay icon to show (e.g. "equipmentTrophyUpgraded" shows the "U" overlay). "none" means no overlay at all, and "tiers" means the default tiers icon with no overlay.
  * @param {string} altName The alt text for the image, used for accessibility and SEO.
  * @param {string} icon The base icon name for the device (e.g. "enhancedAimDrives"). The component will automatically look for this icon in the /public/icons/vehicle_modifiers/equipments/ folder and append ".png".
+ * @param {string} supplySlotIconName Icon overlay for supply slot. ( mobility | firpower | spotting | survivability )
  * @description The overlay image shown on the dropdown menu items. Depends on the device type (e.g. EquipmentPlus_overlay.png).
  * @returns Image container
  */
@@ -14,20 +15,31 @@ export default function MenuItemOverlay({
    overlayType = 'equipmentTrophyBasic',
    altName,
    icon,
+   supplySlotIconName = undefined,
 }: {
    overlayType?: OverlayTypes
    altName: string
    icon: string
+   supplySlotIconName?: string
 }) {
    return (
       <div className='relative w-[60px] h-[60px] flex items-center justify-center'>
-         {overlayType !== 'none' && overlayType !== 'tiers' && (
+         {overlayType !== 'none' && overlayType !== 'tiers' && overlayType !== 'supplySlotActive' && (
             <Image
                src={`/icons/vehicle_modifiers/equipments/${overlayType}_overlay.png`}
                alt={altName}
                width={100}
                height={100}
                className='absolute z-1 max-w-[90px] h-[90px] -top-5 -left-5'
+            />
+         )}
+         {supplySlotIconName && overlayType === 'supplySlotActive' && (
+            <Image
+               src={`/icons/details/${supplySlotIconName}.png`}
+               alt={altName}
+               width={100}
+               height={100}
+               className='absolute z-1 max-w-[25px] h-[25px] -top-0 -left-0'
             />
          )}
          <Image

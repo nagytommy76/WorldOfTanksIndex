d4ca3f1c4a5f31f93652369ddeb49eec4ef08d80
nagytommy76 <nagytommy76@gmail.com>
2026-04-03 20:30:54 +0200

    Add setDeviceModifier, removeDeviceModifier functions to context

--- src/components/VehicleDetails/Context/DevicesContext/DeviceContext.tsx
+++ src/components/VehicleDetails/Context/DevicesContext/DeviceContext.tsx
@@ -1,17 +1,62 @@
 'use client'
-import { createContext, useReducer, useState } from 'react'
+import { createContext, useState } from 'react'
 
-export const DeviceContext = createContext({
-   appliedDeviceModifiers: null,
-})
-
+/**
+ * @param name Name of the equipment modifier, e.g: vehicleCircularVisionRadius
+ * @param value The actual modifier value, e.g: 10% view range increase = 1.1 (or specValue: 1.115)
+ */
 interface IAppliedDeviceModifier {
    name: string
-   specValue: number | null
    value: number
 }
 
-export default function DeviceContextProvider() {
-   const [appliedDeviceModifiers, setAppliedDeviceModifiers] = useState<IAppliedDeviceModifier[] | null>(null)
-   return <div>DeviceContext</div>
+type DeviceModifiers = {
+   [archeType: string]: IAppliedDeviceModifier[]
+}
+
+interface IDeviceContext {
+   appliedDevicesModifiers: DeviceModifiers | null
+   setDeviceModifier(name: string, value: number, archeType: string): void
+   removeDeviceModifier(archeType: string): void
+}
+
+export const DeviceContext = createContext<IDeviceContext>({
+   appliedDevicesModifiers: null,
+   setDeviceModifier() {},
+   removeDeviceModifier() {},
+})
+
+export default function DeviceContextProvider({ children }: { children: React.ReactNode }) {
+   const [appliedDevicesModifiers, setAppliedDevicesModifiers] = useState<DeviceModifiers | null>(null)
+
+   function setDeviceModifier(name: string, value: number, archeType: string) {
+      setAppliedDevicesModifiers((previous) => {
+         const previousForArchetype = previous?.[archeType] ?? []
+         const otherModifiersForArchetype = previousForArchetype.filter((m) => m.name !== name)
+         return {
+            ...previous,
+            [archeType]: [...otherModifiersForArchetype, { name, value }],
+         }
+      })
+   }
+
+   function removeDeviceModifier(archeType: string) {
+      setAppliedDevicesModifiers((previous) => {
+         if (!previous) return null
+         const { [archeType]: _, ...rest } = previous
+         return rest
+      })
+   }
+
+   return (
+      <DeviceContext.Provider
+         value={{
+            appliedDevicesModifiers,
+            setDeviceModifier,
+            removeDeviceModifier,
+         }}
+      >
+         {children}
+      </DeviceContext.Provider>
+   )
 }

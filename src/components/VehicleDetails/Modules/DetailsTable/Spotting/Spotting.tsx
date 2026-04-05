63b6c95498a48d9f827f4f2b24e745555c4518c6
nagytommy76 <nagytommy76@gmail.com>
2026-04-05 20:25:54 +0200

    Display and modify viewRange if modifier activated
    Experimental code -> need to refactor and move to context or reducer

--- src/components/VehicleDetails/Modules/DetailsTable/Spotting/Spotting.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Spotting/Spotting.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { VehicleContext } from '@/VehicleContext/VehicleContext'
+import { DeviceContext } from '@/VehicleContext/DevicesContext/DeviceContext'
 
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
@@ -15,6 +16,45 @@ export default function Spotting() {
          moduleGroup: { vehicleTurret, vehicleRadio },
       },
    } = useContext(VehicleContext)
+   const { appliedDevicesModifiers } = useContext(DeviceContext)
+   const [viewRange, setViewRange] = useState(
+      vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange || 0,
+   )
+
+   useEffect(() => {
+      const viewRangeBase = vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange
+      if (!viewRangeBase) return
+      if (!appliedDevicesModifiers || !appliedDevicesModifiers['coatedOptics']) {
+         setViewRange(viewRangeBase || 0)
+      } else {
+         const modifiersForCoatedOptics = appliedDevicesModifiers['coatedOptics']
+         const visionRadiusModifier = modifiersForCoatedOptics.find(
+            (modifier) => modifier.name === 'vehicleCircularVisionRadius',
+         )
+         if (visionRadiusModifier) {
+            setViewRange((viewRangeBase || 0) * visionRadiusModifier.value)
+         }
+      }
+   }, [appliedDevicesModifiers, vehicleTurret, selectedModuleNames.vehicleTurret])
+
+   function returnAppliedModifierForViewRange() {
+      if (!appliedDevicesModifiers || !appliedDevicesModifiers['coatedOptics']) return null
+      let array = []
+
+      array = appliedDevicesModifiers['coatedOptics'].map((modifier) => {
+         const difference = Number(
+            (
+               modifier.value * vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange -
+               vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange
+            ).toFixed(2),
+         )
+         return {
+            difference: difference,
+            improved: modifier.value > 0,
+         }
+      })
+      return array
+   }
 
    return (
       <Table size='small' aria-label='Spotting table with view range and signal range'>
@@ -27,8 +67,9 @@ export default function Spotting() {
             <TableRowComponent
                iconSrc='/icons/spot/circularVisionRadius.png'
                titleText='View range'
-               valueText={vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange}
+               valueText={parseFloat(viewRange.toFixed(2))}
                unit='m'
+               modifiers={returnAppliedModifierForViewRange()}
             />
             <TableRowComponent
                iconSrc='/icons/spot/radioDistance.png'

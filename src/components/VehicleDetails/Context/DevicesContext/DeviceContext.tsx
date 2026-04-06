c86afc643f4b553e7e8c59fe16c6d1b81137b79a
nagytommy76 <nagytommy76@gmail.com>
2026-04-06 20:20:25 +0200

    Create returnAppliedModifierDiplayValue function
    Pass to DeviceContext

--- src/components/VehicleDetails/Context/DevicesContext/DeviceContext.tsx
+++ src/components/VehicleDetails/Context/DevicesContext/DeviceContext.tsx
@@ -1,110 +1,49 @@
 'use client'
-import { createContext, useState } from 'react'
-import type { DeviceModifiers } from './Types'
-
-interface IDeviceContext {
-   appliedDevicesModifiers: DeviceModifiers | null
-   setDeviceModifier(name: string, value: number, archeType: string): void
-   removeDeviceModifier(archeType: string): void
-}
+import { createContext, useReducer } from 'react'
+import type { DeviceModifierKeys, IDeviceContext } from './Types'
+import { devicesInitialState } from './Types'
+import DevicesReducer from './DevicesReducer'
 
 export const DeviceContext = createContext<IDeviceContext>({
-   appliedDevicesModifiers: null,
-   setDeviceModifier() {},
-   removeDeviceModifier() {},
+   deviceReducer: devicesInitialState,
+   deviceDispatch() {},
+   returnAppliedModifierDiplayValue() {
+      return null
+   },
 })
 
 export default function DeviceContextProvider({ children }: { children: React.ReactNode }) {
-   const [appliedDevicesModifiers, setAppliedDevicesModifiers] = useState<DeviceModifiers | null>(null)
+   const [deviceReducer, deviceDispatch] = useReducer(DevicesReducer, devicesInitialState)
 
-   function setDeviceModifier(name: string, value: number, archeType: string) {
-      setAppliedDevicesModifiers((previous) => {
-         const previousForArchetype = previous?.[archeType] ?? []
-         const otherModifiersForArchetype = previousForArchetype.filter((m) => m.name !== name)
+   function returnAppliedModifierDiplayValue(
+      deviceNamme: DeviceModifierKeys,
+      selectedSpecificationValue: string | number,
+   ) {
+      if (!deviceReducer.appliedDevicesModifiers || !deviceReducer.appliedDevicesModifiers[deviceNamme])
+         return null
+      if (typeof selectedSpecificationValue === 'string')
+         selectedSpecificationValue = parseFloat(selectedSpecificationValue)
+
+      return deviceReducer.appliedDevicesModifiers[deviceNamme].map((modifier) => {
+         const difference = Number(
+            (modifier.value * selectedSpecificationValue - selectedSpecificationValue).toFixed(2),
+         )
          return {
-            ...previous,
-            [archeType]: [...otherModifiersForArchetype, { name, value }],
+            difference: difference,
+            improved: modifier.value > 0,
          }
-      })
-   }
-
-   function removeDeviceModifier(archeType: string) {
-      setAppliedDevicesModifiers((previous) => {
-         if (!previous) return null
-         const { [archeType]: _, ...rest } = previous
-         return rest
       })
    }
 
    return (
       <DeviceContext.Provider
          value={{
-            appliedDevicesModifiers,
-            setDeviceModifier,
-            removeDeviceModifier,
+            deviceDispatch,
+            deviceReducer,
+            returnAppliedModifierDiplayValue,
          }}
       >
          {children}
       </DeviceContext.Provider>
    )
-}
-
-const deviceModifierNames = {
-   tankRammer: ['vehicleGunReloadTime'],
-   antifragmentationLining: [
-      'vehicleHEShellDamageResistance',
-      'vehicleRamDamageResistance',
-      'crewHitChance',
-      'crewStunDuration',
-      'crewRepeatedStunDuration',
-   ],
-   aimingStabilizer: ['vehicleGunShotDispersion'],
-   additionalInvisibilityDevice: ['vehicleCamouflage', 'vehicleCamouflage', 'vehicleCamouflage'],
-   enhancedAimDrives: ['vehicleGunAimSpeed'],
-   extraHealthReserve: [
-      'vehicleStrength',
-      'vehicleChassisStrength',
-      'vehicleChassisRepairSpeed',
-      'vehicleChassisFallDamage',
-   ],
-   improvedRotationMechanism: [
-      'vehicleTurretOrCuttingRotationSpeed',
-      'vehicleAllGroundRotationSpeed',
-      'vehicleGunShotDispersion',
-   ],
-   improvedSights: ['vehicleGunShotFullDispersion'],
-   commandersView: ['demaskFoliageFactor', 'demaskMovingFactor'],
-   grousers: ['vehicleAllGroundRotationSpeed', 'vehicleSpeedGain'],
-   coatedOptics: ['vehicleCircularVisionRadius'],
-   camouflageNet: ['vehicleStillCamouflageDeluxe'],
-   improvedVentilation: ['crewLevel'],
-   turbocharger: ['vehicleEnginePower', 'vehicleForwardMaxSpeed', 'vehicleBackwardMaxSpeed'],
-   improvedConfiguration: [
-      'vehicleRepairSpeed',
-      'vehicleAmmoBayEngineFuelStrength',
-      'vehPenaltyForDamagedEngine',
-      'vehPenaltyForDamagedAmmorack',
-      'vehicleFireChance',
-   ],
-   improvedRadioCommunication: ['vehicleEnemySpottingTime', 'vehicleOwnSpottingTime'],
-   stereoscope: ['vehicleStillCircularVisionRadiusDeluxe, vehicleStillCircularVisionRadius'],
-
-   modernizedTurbochargerRotationMechanism: [
-      'vehicleEnginePower',
-      'vehicleTurretOrCuttingRotationSpeed',
-      'vehicleAllGroundRotationSpeed',
-      'vehicleGunShotDispersion',
-      'vehicleForwardMaxSpeed',
-      'vehicleBackwardMaxSpeed',
-   ],
-   modernizedExtraHealthReserveAntifragmentationLining: [
-      'vehicleStrength',
-      'vehicleAmmoBayEngineFuelStrength',
-      'vehicleChassisStrength',
-      'vehicleChassisRepairSpeed',
-      'vehPenaltyForDamagedEngine',
-      'vehPenaltyForDamagedAmmorack',
-   ],
-   modernizedAimDrivesAimingStabilizer: ['vehicleGunAimSpeed', 'vehicleGunShotDispersion'],
-   modernizedImprovedSightsEnhancedAimDrives: ['vehicleGunShotFullDispersion', 'vehicleGunAimSpeed'],
 }

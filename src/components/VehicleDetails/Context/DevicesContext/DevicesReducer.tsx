a41f44ff4a0646574249abf0f02c5bde6345f8c8
nagytommy76 <nagytommy76@gmail.com>
2026-04-06 20:19:49 +0200

    Add REMOVE_DEVICE_MODIFIER case
    Add DeviceModifiers type

--- src/components/VehicleDetails/Context/DevicesContext/DevicesReducer.tsx
+++ src/components/VehicleDetails/Context/DevicesContext/DevicesReducer.tsx
@@ -1,4 +1,4 @@
-import type { IDevicesReducerState, IDevicesContextActions } from './Types'
+import type { IDevicesReducerState, IDevicesContextActions, DeviceModifiers } from './Types'
 
 export default function DevicesReducer(
    state: IDevicesReducerState,
@@ -14,8 +14,27 @@ export default function DevicesReducer(
             appliedDevicesModifiers: {
                ...state.appliedDevicesModifiers,
                [archeType]: [...otherModifiersForArchetype, { name, value }],
-            },
+            } as DeviceModifiers,
          }
+      }
+      case 'REMOVE_DEVICE_MODIFIER': {
+         const { archeType } = payload
+         if (!state.appliedDevicesModifiers) return state
+         // eslint-disable-next-line @typescript-eslint/no-unused-vars
+         const { [archeType]: _, ...rest } = state.appliedDevicesModifiers
+         if (Object.keys(rest).length === 0) {
+            return {
+               ...state,
+               appliedDevicesModifiers: null,
+            }
+         }
+         return {
+            ...state,
+            appliedDevicesModifiers: rest as DeviceModifiers,
+         }
+      }
+      default: {
+         return state
       }
    }
 }

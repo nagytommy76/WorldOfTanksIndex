105d62cfd7a9b03265e224e61efa605b106b9ec5
nagytommy76 <nagytommy76@gmail.com>
2026-04-06 20:19:03 +0200

    Create IDeviceContext type & moved deviceModifierNames helper object here

--- src/components/VehicleDetails/Context/DevicesContext/Types.ts
+++ src/components/VehicleDetails/Context/DevicesContext/Types.ts
@@ -7,8 +7,10 @@ export interface IAppliedDeviceModifier {
    value: number
 }
 
+export type DeviceModifierKeys = keyof typeof deviceModifierNames
+
 export type DeviceModifiers = {
-   [archeType: string]: IAppliedDeviceModifier[]
+   [archeType in DeviceModifierKeys]: IAppliedDeviceModifier[]
 }
 
 export interface IDevicesReducerState {
@@ -18,10 +20,85 @@ export interface IDevicesReducerState {
 export type IDevicesContextActions =
    | {
         type: 'SET_DEVICE_MODIFIER'
-        payload: { name: string; value: number; archeType: string }
+        payload: { name: string; value: number; archeType: DeviceModifierKeys }
      }
-   | { type: 'REMOVE_DEVICE_MODIFIER'; payload: { archeType: string } }
+   | { type: 'REMOVE_DEVICE_MODIFIER'; payload: { archeType: DeviceModifierKeys } }
 
 export const devicesInitialState: IDevicesReducerState = {
    appliedDevicesModifiers: null,
+}
+
+export interface IDeviceContext {
+   deviceReducer: IDevicesReducerState
+   deviceDispatch: React.Dispatch<IDevicesContextActions>
+   returnAppliedModifierDiplayValue(
+      deviceNamme: DeviceModifierKeys,
+      selectedSpecification: string | number,
+   ):
+      | {
+           difference: number
+           improved: boolean
+        }[]
+      | null
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const deviceModifierNames = {
+   tankRammer: ['vehicleGunReloadTime'],
+   antifragmentationLining: [
+      'vehicleHEShellDamageResistance',
+      'vehicleRamDamageResistance',
+      'crewHitChance',
+      'crewStunDuration',
+      'crewRepeatedStunDuration',
+   ],
+   aimingStabilizer: ['vehicleGunShotDispersion'],
+   additionalInvisibilityDevice: ['vehicleCamouflage', 'vehicleCamouflage', 'vehicleCamouflage'],
+   enhancedAimDrives: ['vehicleGunAimSpeed'],
+   extraHealthReserve: [
+      'vehicleStrength',
+      'vehicleChassisStrength',
+      'vehicleChassisRepairSpeed',
+      'vehicleChassisFallDamage',
+   ],
+   improvedRotationMechanism: [
+      'vehicleTurretOrCuttingRotationSpeed',
+      'vehicleAllGroundRotationSpeed',
+      'vehicleGunShotDispersion',
+   ],
+   improvedSights: ['vehicleGunShotFullDispersion'],
+   commandersView: ['demaskFoliageFactor', 'demaskMovingFactor'],
+   grousers: ['vehicleAllGroundRotationSpeed', 'vehicleSpeedGain'],
+   coatedOptics: ['vehicleCircularVisionRadius'],
+   camouflageNet: ['vehicleStillCamouflageDeluxe'],
+   improvedVentilation: ['crewLevel'],
+   turbocharger: ['vehicleEnginePower', 'vehicleForwardMaxSpeed', 'vehicleBackwardMaxSpeed'],
+   improvedConfiguration: [
+      'vehicleRepairSpeed',
+      'vehicleAmmoBayEngineFuelStrength',
+      'vehPenaltyForDamagedEngine',
+      'vehPenaltyForDamagedAmmorack',
+      'vehicleFireChance',
+   ],
+   improvedRadioCommunication: ['vehicleEnemySpottingTime', 'vehicleOwnSpottingTime'],
+   stereoscope: ['vehicleStillCircularVisionRadiusDeluxe, vehicleStillCircularVisionRadius'],
+
+   modernizedTurbochargerRotationMechanism: [
+      'vehicleEnginePower',
+      'vehicleTurretOrCuttingRotationSpeed',
+      'vehicleAllGroundRotationSpeed',
+      'vehicleGunShotDispersion',
+      'vehicleForwardMaxSpeed',
+      'vehicleBackwardMaxSpeed',
+   ],
+   modernizedExtraHealthReserveAntifragmentationLining: [
+      'vehicleStrength',
+      'vehicleAmmoBayEngineFuelStrength',
+      'vehicleChassisStrength',
+      'vehicleChassisRepairSpeed',
+      'vehPenaltyForDamagedEngine',
+      'vehPenaltyForDamagedAmmorack',
+   ],
+   modernizedAimDrivesAimingStabilizer: ['vehicleGunAimSpeed', 'vehicleGunShotDispersion'],
+   modernizedImprovedSightsEnhancedAimDrives: ['vehicleGunShotFullDispersion', 'vehicleGunAimSpeed'],
 }

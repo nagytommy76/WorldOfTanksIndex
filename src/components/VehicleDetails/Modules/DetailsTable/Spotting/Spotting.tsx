2bc237f6d20ecb5fe9aec4c0d0c9389a6a0d418f
nagytommy76 <nagytommy76@gmail.com>
2026-04-12 20:03:37 +0200

    Add Time being spotted and CVs taleRow

--- src/components/VehicleDetails/Modules/DetailsTable/Spotting/Spotting.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Spotting/Spotting.tsx
@@ -9,6 +9,7 @@ import TableBody from '@mui/material/TableBody'
 import TableHeadComponent from '../Includes/TableHead'
 import TableRowComponent from '../Includes/TableRow'
 
+const SPOTTED_TIME = 10
 export default function Spotting() {
    const {
       vehicleReducer: {
@@ -22,14 +23,25 @@ export default function Spotting() {
       setAppliedDeviceModifier,
    } = useContext(DeviceContext)
 
-   const [viewRange, setViewRange] = useState(
-      vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange || 0,
-   )
+   const viewRangeBase = vehicleTurret[selectedModuleNames.vehicleTurret].viewRange
+
+   const [viewRange, setViewRange] = useState(vehicleTurret[selectedModuleNames.vehicleTurret].viewRange)
+   const [timeBeingSpotted, setTimeBeingSpotted] = useState(SPOTTED_TIME)
+   const [timeEnemySpotted, setTimeEnemySpotted] = useState(SPOTTED_TIME)
 
    useEffect(() => {
-      const viewRangeBase = vehicleTurret[selectedModuleNames.vehicleTurret]?.viewRange
       setAppliedDeviceModifier(viewRangeBase, 'coatedOptics', 'vehicleCircularVisionRadius', setViewRange)
-   }, [selectedModuleNames, vehicleTurret, appliedDevicesModifiers, setAppliedDeviceModifier])
+   }, [selectedModuleNames, vehicleTurret, viewRangeBase, appliedDevicesModifiers, setAppliedDeviceModifier])
+
+   useEffect(() => {
+      if (!appliedDevicesModifiers || !appliedDevicesModifiers['improvedRadioCommunication']) {
+         setTimeBeingSpotted(SPOTTED_TIME)
+         setTimeEnemySpotted(SPOTTED_TIME)
+         return
+      }
+      setTimeBeingSpotted(SPOTTED_TIME - appliedDevicesModifiers['improvedRadioCommunication'][0].value)
+      setTimeEnemySpotted(SPOTTED_TIME + appliedDevicesModifiers['improvedRadioCommunication'][0].value)
+   }, [appliedDevicesModifiers])
 
    return (
       <Table size='small' aria-label='Spotting table with view range and signal range'>
@@ -58,15 +70,47 @@ export default function Spotting() {
             <TableRowComponent
                iconSrc='/icons/spot/vehicleOwnSpottingTime.png'
                titleText='Time being spotted'
-               valueText={10}
+               valueText={timeBeingSpotted}
                unit='seconds'
+               modifiers={[
+                  {
+                     difference: timeBeingSpotted - SPOTTED_TIME,
+                     improved: true,
+                  },
+               ]}
             />
             <TableRowComponent
                iconSrc='/icons/spot/vehicleEnemySpottingTime.png'
                titleText='Time enemy remains spotted'
-               valueText={10}
+               valueText={timeEnemySpotted}
                unit='seconds'
+               modifiers={[
+                  {
+                     difference: timeEnemySpotted - SPOTTED_TIME,
+                     improved: true,
+                  },
+               ]}
             />
+            {appliedDevicesModifiers && appliedDevicesModifiers['commandersView'] && (
+               <>
+                  <TableRowComponent
+                     iconSrc='/icons/spot/demaskFoliageFactor.png'
+                     titleText='Concealment of enemy vehicles behind foliage'
+                     valueText={parseFloat(
+                        ((appliedDevicesModifiers['commandersView'][0].value - 1) * 100).toFixed(2),
+                     )}
+                     unit='%'
+                  />
+                  <TableRowComponent
+                     iconSrc='/icons/spot/demaskMovingFactor.png'
+                     titleText='Concealment of moving enemy vehicles'
+                     valueText={parseFloat(
+                        ((appliedDevicesModifiers['commandersView'][1].value - 1) * 100).toFixed(2),
+                     )}
+                     unit='%'
+                  />
+               </>
+            )}
          </TableBody>
       </Table>
    )

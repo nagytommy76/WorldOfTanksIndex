05c8bd44cd38d4cb8222e64a7935ddde5bf406f2
nagytommy76 <nagytommy76@gmail.com>
2026-05-02 19:59:47 +0200

    Using reducer & Fill crewMembers reducer object with crew members

--- src/components/VehicleDetails/Context/CrewContext/CrewContext.tsx
+++ src/components/VehicleDetails/Context/CrewContext/CrewContext.tsx
@@ -1,11 +1,21 @@
 'use client'
-import { createContext } from 'react'
+import { createContext, useReducer, useEffect } from 'react'
 
 import type { ICrewMembers } from '@/types/VehicleDetails/Crew'
+import type { CrewMembersType, ICrewContext } from './Types'
+import { crewInitialState, initialCrewMembers } from './Types'
 
-import Crew from './Classes/Crew'
+import CrewMember from './Classes/Crew'
 
-export const CrewContext = createContext({})
+import CrewReducer from './CrewReducer'
+
+export const CrewContext = createContext<ICrewContext>({
+   crewDispatch: () => null,
+   crewReducer: {
+      crewMode: 'base',
+      crewMembers: initialCrewMembers,
+   },
+})
 
 export default function CrewContextProvider({
    children,
@@ -14,12 +24,28 @@ export default function CrewContextProvider({
    children: React.ReactNode
    crewMembers: ICrewMembers[]
 }) {
-   for (const member of crewMembers) {
-      const crew = new Crew({
-         primaryRole: member.primary,
-         secondaryRole: member.secondary,
-      })
-   }
+   const [crewReducer, crewDispatch] = useReducer(CrewReducer, crewInitialState)
 
-   return <CrewContext.Provider value={{}}>{children}</CrewContext.Provider>
+   useEffect(() => {
+      const crewHelperObject = initialCrewMembers as CrewMembersType
+      for (const member of crewMembers) {
+         const crewMember = new CrewMember({
+            primaryRole: member.primary,
+            secondaryRole: member.secondary,
+         })
+         crewHelperObject[crewMember.primaryRole] = crewMember
+      }
+      crewDispatch({ type: 'ADD_INITIAL_CREW', payload: crewHelperObject })
+   }, [crewMembers])
+
+   return (
+      <CrewContext.Provider
+         value={{
+            crewDispatch,
+            crewReducer,
+         }}
+      >
+         {children}
+      </CrewContext.Provider>
+   )
 }

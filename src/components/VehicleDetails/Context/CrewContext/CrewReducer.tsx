2dd396f11e59923f5af49b7b2b59c8e1d1c1948f
nagytommy76 <nagytommy76@gmail.com>
2026-05-11 20:12:56 +0200

    Using setAppliedCrewModifier & removeAppliedCrewModifier object properties

--- src/components/VehicleDetails/Context/CrewContext/CrewReducer.tsx
+++ src/components/VehicleDetails/Context/CrewContext/CrewReducer.tsx
@@ -8,16 +8,23 @@ export default function CrewReducer(
       case 'TOGGLE_CREW_MODE':
          for (const [role, member] of Object.entries(state.crewMembers)) {
             if (!member) continue
-
-            const newEfficiencyLevel = member.computeEfficiencyLevel([], state.crewMode === 'base')
-            // console.log('ROLE: ', role)
-            // console.log(role, newEfficiencyLevel)
-            member.efficiencyLevel = newEfficiencyLevel
+            if (role === 'commander' && action.payload) continue
+            if (action.payload) {
+               member.setAppliedCrewModifier({
+                  name: 'commanderBonus',
+                  paramName: 'commanderBonus',
+                  value: 0.1,
+               })
+            } else {
+               member.removeAppliedCrewModifier('commanderBonus')
+            }
          }
          return {
             ...state,
             crewMode: state.crewMode === 'base' ? 'effective' : 'base',
          }
+      case 'SET_APPLIED_CREW_MODIFIER':
+         return state
       case 'ADD_INITIAL_CREW':
          return {
             ...state,

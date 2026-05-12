0abc818b17b99a4ee050de9ea0868fd663e290c1
nagytommy76 <nagytommy76@gmail.com>
2026-05-12 19:51:35 +0200

    Add SET_APPLIED_CREW_MODIFIER & REMOVE_APPLIED_CREW_MODIFIER cases

--- src/components/VehicleDetails/Context/CrewContext/CrewReducer.tsx
+++ src/components/VehicleDetails/Context/CrewContext/CrewReducer.tsx
@@ -5,26 +5,51 @@ export default function CrewReducer(
    action: ICrewContextActions,
 ): ICrewReducerState {
    switch (action.type) {
-      case 'TOGGLE_CREW_MODE':
-         for (const [role, member] of Object.entries(state.crewMembers)) {
+      case 'TOGGLE_COMMANDER_BONUS':
+         const newToggleCrewMembers = state.crewMembers
+         for (const [role, member] of Object.entries(newToggleCrewMembers)) {
             if (!member) continue
             if (role === 'commander' && action.payload) continue
             if (action.payload) {
                member.setAppliedCrewModifier({
                   name: 'commanderBonus',
                   paramName: 'commanderBonus',
-                  value: 0.1,
+                  value: 1.1,
                })
             } else {
                member.removeAppliedCrewModifier('commanderBonus')
             }
          }
+
          return {
             ...state,
+            crewMembers: { ...newToggleCrewMembers },
             crewMode: state.crewMode === 'base' ? 'effective' : 'base',
          }
       case 'SET_APPLIED_CREW_MODIFIER':
-         return state
+         const newCrewMembers = state.crewMembers
+         for (const member of Object.values(newCrewMembers)) {
+            if (!member) continue
+            member.setAppliedCrewModifier({
+               name: action.payload.name,
+               paramName: action.payload.name,
+               value: action.payload.value,
+            })
+         }
+         return {
+            ...state,
+            crewMembers: { ...newCrewMembers },
+         }
+      case 'REMOVE_APPLIED_CREW_MODIFIER':
+         const newRemoveCrewMembers = state.crewMembers
+         for (const member of Object.values(newRemoveCrewMembers)) {
+            if (!member) continue
+            member.removeAppliedCrewModifier(action.payload)
+         }
+         return {
+            ...state,
+            crewMembers: { ...newRemoveCrewMembers },
+         }
       case 'ADD_INITIAL_CREW':
          return {
             ...state,

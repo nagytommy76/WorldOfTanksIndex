c1e8c89bab049198a4ca37a9b1ba27ef63bc5abc
nagytommy76 <nagytommy76@gmail.com>
2026-05-19 19:56:43 +0200

    Using HtmlTooltip & TooltipContent

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/CrewSkills.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/CrewSkills.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 
 import type { CrewSkillRoles } from '@/Classes/CrewSkills'
-
 import useGetCrewSkills from './Hooks/useGetCrewSkills'
 
+import HtmlTooltip from '@/helpers/HtmlTooltip'
+import TooltipContent from './Includes/TooltipContent'
+
 import Typography from '@mui/material/Typography'
-import Tooltip from '@mui/material/Tooltip'
 
 export default function CrewSkills() {
    const crewSkills = useGetCrewSkills()
@@ -25,17 +26,29 @@ export default function CrewSkills() {
          <Typography variant='h6'>Crew Skills</Typography>
          <section className='flex flex-row'>
             {Object.entries(sortedCrew).map(([role, skills]) => (
-               <div className='flex flex-col' key={role}>
+               <div className='flex flex-col gap-2' key={role}>
                   {skills.map((skill) => (
-                     <Tooltip disableInteractive key={skill.xmlName} title={skill.description}>
+                     <HtmlTooltip
+                        key={skill.xmlName}
+                        placement='top'
+                        title={
+                           <TooltipContent
+                              skillName={skill.name || 'Unknown'}
+                              description={skill.description}
+                              iconName={skill.xmlName}
+                              isSituational={skill.typeName === 'situational'}
+                           />
+                        }
+                        disableInteractive
+                     >
                         <Image
                            src={`/crewSkills/${skill.xmlName}.png`}
                            alt={skill.xmlName}
-                           width={60}
-                           height={60}
+                           width={40}
+                           height={40}
                            className=''
                         />
-                     </Tooltip>
+                     </HtmlTooltip>
                   ))}
                </div>
             ))}

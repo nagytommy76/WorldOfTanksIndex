59afea62ed28121b1c0b9bf08d1470306f6ca23a
nagytommy76 <nagytommy76@gmail.com>
2026-05-27 19:59:25 +0200

    Cleanup
    Remove div, onClick function -> moved to ToggleCrewSkills component

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/Includes/SingleCrewSkill.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/Includes/SingleCrewSkill.tsx
@@ -1,60 +1,24 @@
 import Image from 'next/image'
-import { useContext, useState } from 'react'
-import { CrewContext } from '@/VehicleDetails/Context/CrewContext/CrewContext'
 
-import CrewSkills, { type ICrewRoles } from '@/Classes/CrewSkills'
+import CrewSkills from '@/Classes/CrewSkills'
 import HtmlTooltip from '@/helpers/HtmlTooltip'
 import TooltipContent from '../Includes/TooltipContent'
 
-export default function SingleCrewSkill({ skill, role }: { skill: CrewSkills; role: ICrewRoles }) {
-   const [checked, setChecked] = useState(false)
-   const {
-      crewDispatch,
-      crewReducer: { crewMembers },
-   } = useContext(CrewContext)
-   function handleClick() {
-      if (!checked) {
-         if (skill.xmlName === 'brotherhood' || skill.modifiers[0].paramName === 'crewLevelIncrease') {
-            crewDispatch({
-               type: 'SET_APPLIED_CREW_MODIFIER',
-               payload: {
-                  name: skill.xmlName,
-                  value: skill.modifiers[0].value,
-               },
-            })
-         } else {
-            crewDispatch({
-               type: 'SET_APPLIED_CREW_SKILLS',
-               payload: {
-                  appliedSkillName: skill.xmlName,
-                  crewSkillModifiers: skill.modifiers,
-                  role,
-               },
-            })
-         }
-      } else {
-         console.log('CHECKED SO REMOVE SKILL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-      }
-
-      setChecked((prevChecked) => !prevChecked)
-   }
-
+export default function SingleCrewSkill({ skill }: { skill: CrewSkills }) {
    return (
-      <div onClick={handleClick}>
-         <HtmlTooltip
-            key={skill.xmlName}
-            placement='top'
-            title={<TooltipContent skill={skill} />}
-            disableInteractive
-         >
-            <Image
-               src={`/crewSkills/${skill.xmlName}.png`}
-               alt={skill.xmlName}
-               width={40}
-               height={40}
-               className='w-[40px] h-[40px] object-cover'
-            />
-         </HtmlTooltip>
-      </div>
+      <HtmlTooltip
+         key={skill.xmlName}
+         placement='top'
+         title={<TooltipContent skill={skill} />}
+         disableInteractive
+      >
+         <Image
+            src={`/crewSkills/${skill.xmlName}.png`}
+            alt={skill.xmlName}
+            width={40}
+            height={40}
+            className='w-[40px] h-[40px] object-cover'
+         />
+      </HtmlTooltip>
    )
 }

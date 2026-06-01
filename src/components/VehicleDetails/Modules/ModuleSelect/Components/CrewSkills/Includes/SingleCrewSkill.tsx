e04981b147616f8c58a37801d7ef175e15a4279e
nagytommy76 <nagytommy76@gmail.com>
2026-06-01 19:49:34 +0200

    Add isDisabled property

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/Includes/SingleCrewSkill.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/Includes/SingleCrewSkill.tsx
@@ -4,7 +4,13 @@ import CrewSkills from '@/Classes/CrewSkills'
 import HtmlTooltip from '@/helpers/HtmlTooltip'
 import TooltipContent from '../Includes/TooltipContent'
 
-export default function SingleCrewSkill({ skill }: { skill: CrewSkills }) {
+export default function SingleCrewSkill({
+   skill,
+   isDisabled = false,
+}: {
+   skill: CrewSkills
+   isDisabled?: boolean
+}) {
    return (
       <HtmlTooltip
          key={skill.xmlName}
@@ -17,7 +23,7 @@ export default function SingleCrewSkill({ skill }: { skill: CrewSkills }) {
             alt={skill.xmlName}
             width={40}
             height={40}
-            className='w-[40px] h-[40px] object-cover'
+            className={`w-[40px] h-[40px] object-cover ${isDisabled ? 'opacity-40' : 'opacity-100'}`}
          />
       </HtmlTooltip>
    )

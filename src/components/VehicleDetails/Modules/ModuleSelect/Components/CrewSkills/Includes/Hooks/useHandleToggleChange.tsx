a818e256a77bf5e37354059486610aa596e85e9d
nagytommy76 <nagytommy76@gmail.com>
2026-06-01 19:48:04 +0200

    Set newFormats -> removed exclusive from ToggleButtonGroup

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/Includes/Hooks/useHandleToggleChange.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/CrewSkills/Includes/Hooks/useHandleToggleChange.tsx
@@ -3,13 +3,8 @@ import { useState } from 'react'
 export default function useHandleToggleChange() {
    const [selectedSkills, setSelectedSkills] = useState<string[]>(() => [])
 
-   function handleToggleChancge(event: React.MouseEvent<HTMLElement>, newFormats: string) {
-      if (selectedSkills.includes(newFormats)) {
-         const filteredCrewSkill = selectedSkills.filter((skill) => skill !== newFormats)
-         setSelectedSkills(filteredCrewSkill)
-      } else {
-         setSelectedSkills((prevSelectedSkills) => [...prevSelectedSkills, newFormats])
-      }
+   function handleToggleChancge(event: React.MouseEvent<HTMLElement>, newFormats: string[]) {
+      setSelectedSkills(newFormats)
    }
    return { handleToggleChancge, selectedSkills }
 }

76f8992ec5a9e68039fde152d8e76d75a65c8196
nagytommy76 <nagytommy76@gmail.com>
2026-06-24 19:48:53 +0200

    Calculation crew skill scale based on efficiency level

--- src/utils/ApplyCrewSkillModifier.ts
+++ src/utils/ApplyCrewSkillModifier.ts
@@ -4,6 +4,20 @@ import Commander from '@/CrewContext/Classes/Commander'
 
 import type { StatTransformer } from './applyStatPipeline'
 
+/**
+ * 
+ * @param crewMember 
+ * @returns calculatedSkillResult - 
+ * @example
+ *    Recon's base bonus = 2%
+ * 
+      baseBonus         = 2% view range
+      effectiveQual     = 110%
+      scaledBonus       = 2% × (110 / 100) = 2.2%
+
+      Applied to view range:
+      406.77m × (1 + 0.022) = 406.71 × 1.022 ≈ 415.71894m ≈ 415.72m
+ */
 export default function createCrewSkillsTransformer<T extends Record<string, number>>(
    crewMember: CrewMember | Commander | undefined,
 ): StatTransformer<T> {
@@ -28,46 +42,29 @@ export default function createCrewSkillsTransformer<T extends Record<string, num
 
                switch (config.measureType) {
                   case 'percents':
-                     /**
-                      * A probléma:
-                      *
-                      * effectiveSkill = (primarySkill + ventilationBonus) + ((commanderSkill + ventilationBonus) * 0.1)
-                      *
-                      * (100 + 5) + ((100 + 5) * 0.1) = 105 + 10.5 = 115.5%
-                      *
-                      * tehát a commander bonus-t a commander efficiency level alapján kell hozzáadni a többi crew memberhaz
-                      *
-                      */
-
+                     const scaledBonus = (skill.value - 1) * (crewMember.efficiencyLevel / 100)
                      /**
                       * In this case I check if a crewMember is !Commander
                       * and isCommanderBonusApplied is true (+10% bonus switch turned on)
                       */
-                     if (crewMember instanceof CrewMember && crewMember.isCommanderBonusApplied) {
-                        switch (config.operation) {
-                           case 'degressive':
-                              console.log(`${crewMember.primaryRole} has ${calculatedSkillResult[key]}`)
-                              ;(calculatedSkillResult[key] as number) =
-                                 (calculatedSkillResult[key] * 0.875) /
-                                 (0.00375 * crewMember.efficiencyLevel + 0.5)
-                              calculatedSkillResult[key] /= skill.value
+                     // if (crewMember instanceof CrewMember && crewMember.isCommanderBonusApplied) {
+                     switch (config.operation) {
+                        case 'degressive':
+                           console.log(`${crewMember.primaryRole} has ${calculatedSkillResult[key]}`)
+                           // ;(calculatedSkillResult[key] as number) =
+                           //    (calculatedSkillResult[key] * 0.875) /
+                           //    (0.00375 * crewMember.efficiencyLevel + 0.5)
+                           // calculatedSkillResult[key] /= skill.value
+                           calculatedSkillResult[key] /= 1 + scaledBonus
 
-                              break
-                        }
-                     } else {
-                        calculatedSkillResult[key] *= skill.value
-                        console.log('ELSE APPLY CREW')
+                           break
+                        case 'progressive':
+                           calculatedSkillResult[key] *= 1 + scaledBonus
+                           break
                      }
-
-                     // const final = skill.value / 100 + multiplyValue
-                     // console.log('MULTIPLYED 2222: ', final)
-
-                     //    calculatedSkillResult[key] *= skill.value
                      // } else {
-                     // ;(calculatedSkillResult[key] as number) =
-                     //    (calculatedSkillResult[key] / 0.875) * (0.00375 * crewMember.efficiencyLevel + 0.5)
-                     // console.log('CSÁÁÁÉÉ:::  ', calculatedSkillResult[key])
-
+                     //    calculatedSkillResult[key] *= 1 + scaledBonus
+                     //    console.log('ELSE APPLY CREW: ', scaledBonus)
                      // }
 
                      break
@@ -79,11 +76,3 @@ export default function createCrewSkillsTransformer<T extends Record<string, num
       return calculatedSkillResult
    }
 }
-
-/**
- *
- * Vehicle attributes instead scale using the formula
- * 0.57 + (0.43 × crewLevel). For example, if your commander is at 120%,
- * your view range will be buffed by 0.57 + (0.43 × 1.2) = 1.086 = +8.6%.
- *  A handful of skills also use this formula.
- */

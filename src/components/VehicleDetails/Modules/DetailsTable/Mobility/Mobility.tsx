7f1d817cacbbfcfa3afb7664b4e2d85862cc13cf
nagytommy76 <nagytommy76@gmail.com>
2026-06-28 18:53:45 +0200

    Add driver to createCrewSkillsTransformer

--- src/components/VehicleDetails/Modules/DetailsTable/Mobility/Mobility.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Mobility/Mobility.tsx
@@ -83,6 +83,7 @@ export default function Mobility() {
                createCrewTransformer(crewMembers.driver),
                createCrewTransformer(crewMembers.gunner),
                createCrewSkillsTransformer(crewMembers.gunner),
+               createCrewSkillsTransformer(crewMembers.driver),
             ],
          ),
       [

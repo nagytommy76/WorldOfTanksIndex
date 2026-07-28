afa760d16a22cbb92285ef3879e3456c2bb9886b
nagytommy76 <nagytommy76@gmail.com>
2026-07-28 19:49:38 +0200

    Use single tab & custom tab panel components

--- src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/ModuleSelect.tsx
@@ -1,14 +1,13 @@
 'use client'
 import { useState } from 'react'
-import Image from 'next/image'
-
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
 
 import Modules from './Components/Modules'
 import Shells from './Components/Shells'
 import Devices from './Components/Devices/Devices'
 import CrewSkills from './Components/CrewSkills/CrewSkills'
+
+import SingleTab from './Includes/ModuleTabs/SingleTab'
+import CustomTabPanel from './Includes/ModuleTabs/CustomTabPanel'
 
 const Pages = {
    modules: {
@@ -41,71 +40,34 @@ export default function ModuleSelect() {
    }
 
    return (
-      <>
-         <aside className={'flex flex-col items-center gap-4 py-5 xl:py-0 xl:w-[300px]'}>
-            <div className='max-w-full flex flex-row gap-3'>
-               {Object.entries(Pages).map(([pageName, data], index) => {
-                  return (
-                     <>
-                        <Image
-                           src={data.imgSrc}
-                           alt={`${pageName}-image`}
-                           width={50}
-                           height={50}
-                           onClick={(event) => handleChange(event, index)}
-                           className={`
-                           cursor-pointer
-                           transition-colors
-                           hover:bg-neutral-800
-                           ${value === index ? 'bg-amber-300' : ''} 
-                           `}
-                        />
-                     </>
-                  )
-               })}
-            </div>
-            <CustomTabPanel value={value} index={0}>
-               <Modules />
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={1}>
-               <Shells />
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={2}>
-               <Devices />
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={3}>
-               <CrewSkills />
-            </CustomTabPanel>
-         </aside>
-      </>
+      <aside className={'flex flex-col items-center gap-4 py-5 xl:py-0 xl:w-[300px]'}>
+         <div className='w-[275px] flex justify-center my-4 '>
+            {Object.entries(Pages).map(([pageName, data], index) => {
+               return (
+                  <SingleTab
+                     key={index}
+                     title={data.title}
+                     imgSrc={data.imgSrc}
+                     pageName={pageName}
+                     index={index}
+                     handleChange={handleChange}
+                     value={value}
+                  />
+               )
+            })}
+         </div>
+         <CustomTabPanel value={value} index={0}>
+            <Modules />
+         </CustomTabPanel>
+         <CustomTabPanel value={value} index={1}>
+            <Shells />
+         </CustomTabPanel>
+         <CustomTabPanel value={value} index={2}>
+            <Devices />
+         </CustomTabPanel>
+         <CustomTabPanel value={value} index={3}>
+            <CrewSkills />
+         </CustomTabPanel>
+      </aside>
    )
-}
-
-interface TabPanelProps {
-   children?: React.ReactNode
-   index: number
-   value: number
-}
-
-function CustomTabPanel(props: TabPanelProps) {
-   const { children, value, index, ...other } = props
-
-   return (
-      <div
-         role='tabpanel'
-         hidden={value !== index}
-         id={`simple-tabpanel-${index}`}
-         aria-labelledby={`simple-tab-${index}`}
-         {...other}
-      >
-         {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-      </div>
-   )
-}
-
-function a11yProps(index: number) {
-   return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-   }
 }

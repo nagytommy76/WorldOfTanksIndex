b574da616160bf49ed08cd833074c276fdfdb8b7
nagytommy76 <nagytommy76@gmail.com>
2026-07-29 19:52:27 +0200

    Add section with width styles

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/Modules.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/Modules.tsx
@@ -7,6 +7,9 @@ import SingleModuleElement from '../Includes/SingleModuleElement'
 import ReturnTypography from '../Includes/ModuleType'
 
 import useSetModuleName from '../Hooks/useSetModuleName'
+import ModuleTypeTypography from '../Includes/ModuleTypeTypography'
+
+import Typography from '@mui/material/Typography'
 
 export default function Modules() {
    const {
@@ -16,27 +19,35 @@ export default function Modules() {
    const { setModuleName, setModuleNameByType } = useSetModuleName(vehicleDispatch)
 
    return (
-      <>
-         <ReturnTypography text='Modules' />
-         <section className='flex flex-row items-start justify-center gap-2'>
+      <section className='w-[300px] xl:w-[250px] p-2'>
+         <ReturnTypography text='Modules' variant='h4' />
+         <section className='flex flex-col items-start justify-center gap-2'>
             {(Object.entries(moduleGroup) as KeyValuePairs<typeof moduleGroup>).map(([key, modules]) => (
-               <section key={key} className='flex flex-col gap-3'>
-                  {key !== 'shells' &&
-                     (Object.entries(modules) as KeyValuePairs<typeof modules>).map(([moduleKey, module]) => (
-                        <SingleModuleElement
-                           key={moduleKey}
-                           module={module}
-                           moduleName={setModuleName(module.name)}
-                           moduleType={key}
-                           selected={module.name === selectedModuleNames[key]}
-                           onClickFn={() => {
-                              setModuleNameByType(key, module.name)
-                           }}
-                        />
-                     ))}
+               <section key={key} className='flex flex-col gap-3 w-full'>
+                  {key !== 'shells' && (
+                     <>
+                        <Typography variant='h6' fontSize={17} gutterBottom color='textSecondary'>
+                           {ModuleTypeTypography(key)}
+                        </Typography>
+                        {(Object.entries(modules) as KeyValuePairs<typeof modules>).map(
+                           ([moduleKey, module]) => (
+                              <SingleModuleElement
+                                 key={moduleKey}
+                                 module={module}
+                                 moduleName={setModuleName(module.name)}
+                                 moduleType={key}
+                                 selected={module.name === selectedModuleNames[key]}
+                                 onClickFn={() => {
+                                    setModuleNameByType(key, module.name)
+                                 }}
+                              />
+                           ),
+                        )}
+                     </>
+                  )}
                </section>
             ))}
          </section>
-      </>
+      </section>
    )
 }

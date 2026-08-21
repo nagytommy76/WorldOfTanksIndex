8b82bc0247abec631f8bab2a419d4bdab92cc2a3
nagytommy76 <nagytommy76@gmail.com>
2026-08-21 19:44:04 +0200

    Use useSelected hook

--- src/components/VehicleDetails/Modules/ModuleSelect/Components/BattleBoosters/SingleBooster.tsx
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/BattleBoosters/SingleBooster.tsx
@@ -1,73 +1,14 @@
-import { useContext, useEffect, useState } from 'react'
 import Image from 'next/image'
-import { DeviceContext } from '@/DevicesContext/DeviceContext'
-import { BattleBoosterModifierKeys } from '@/DevicesContext/Types'
+import useSelected from './Hooks/useSelected'
+
+import type { IDevice } from '@/types/Devices/Devices'
 
 import Button from '@mui/material/Button'
 import Badge from '@mui/material/Badge'
-import type { IDevice } from '@/types/Devices/Devices'
 import CheckIcon from '@mui/icons-material/Check'
 
 export default function SingleBooster({ booster }: { booster: IDevice }) {
-   const [isBlocked, setIsBolcked] = useState(true)
-   const [isSelected, setISSelected] = useState(true)
-   const {
-      deviceDispatch,
-      deviceReducer: { incompatibleDevices, appliedBattleBoosterModifiers },
-      selectedDevices,
-   } = useContext(DeviceContext)
-
-   useEffect(() => {
-      if (!incompatibleDevices) {
-         setIsBolcked(true)
-         return
-      }
-      const foundInAppliedModifiers =
-         incompatibleDevices.includes(booster.icon) || Object.keys(selectedDevices).includes(booster.icon)
-
-      if (foundInAppliedModifiers) {
-         setIsBolcked(!foundInAppliedModifiers)
-      } else {
-         setIsBolcked(true)
-         deviceDispatch({
-            type: 'REMOVE_BATTLE_BOOSTER_MODIFIER',
-            payload: {
-               archeType: booster.icon as BattleBoosterModifierKeys,
-            },
-         })
-         setISSelected(true)
-      }
-   }, [incompatibleDevices, booster.icon, selectedDevices, deviceDispatch])
-
-   function AddRemoveBooster() {
-      if (booster.modifiers) {
-         if (
-            appliedBattleBoosterModifiers &&
-            Object.keys(appliedBattleBoosterModifiers).includes(booster.icon)
-         ) {
-            deviceDispatch({
-               type: 'REMOVE_BATTLE_BOOSTER_MODIFIER',
-               payload: {
-                  archeType: booster.icon as BattleBoosterModifierKeys,
-               },
-            })
-            setISSelected(true)
-         } else {
-            booster.modifiers.forEach((modifier) => {
-               deviceDispatch({
-                  type: 'SET_BATTLE_BOOSTER_MODIFIER',
-                  payload: {
-                     archeType: booster.icon as BattleBoosterModifierKeys,
-                     name: modifier.name,
-                     value: modifier.value,
-                  },
-               })
-               setISSelected(false)
-            })
-         }
-      }
-   }
-
+   const { AddRemoveBooster, isBlocked, isSelected } = useSelected(booster)
    return (
       <Button
          disabled={isBlocked}

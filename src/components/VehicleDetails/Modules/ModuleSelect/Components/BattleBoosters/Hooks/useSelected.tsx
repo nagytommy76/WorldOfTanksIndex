9ec6963200a2d6134c79ae06172ff9f7f45e3dc6
nagytommy76 <nagytommy76@gmail.com>
2026-08-21 19:43:07 +0200

    Create custom hook -> Checks if incompatibleDevices is null

---
+++ src/components/VehicleDetails/Modules/ModuleSelect/Components/BattleBoosters/Hooks/useSelected.tsx
@@ -0,0 +1,102 @@
+import { useContext, useEffect, useState } from 'react'
+import { DeviceContext } from '@/DevicesContext/DeviceContext'
+import { CrewContext } from '@/CrewContext/CrewContext'
+
+import type { BattleBoosterModifierKeys } from '@/DevicesContext/Types'
+import type { IDevice } from '@/types/Devices/Devices'
+
+export default function useSelected(booster: IDevice) {
+   const [isBlocked, setIsBolcked] = useState(true)
+   const [isSelected, setISSelected] = useState(true)
+
+   const { crewDispatch } = useContext(CrewContext)
+   const {
+      deviceDispatch,
+      deviceReducer: { incompatibleDevices, appliedBattleBoosterModifiers },
+      selectedDevices,
+   } = useContext(DeviceContext)
+
+   /**
+    * @description Checks if incompatibleDevices is null -> set blocked
+    */
+   useEffect(() => {
+      if (!incompatibleDevices) {
+         setIsBolcked(true)
+         return
+      }
+      const foundInAppliedModifiers =
+         incompatibleDevices.includes(booster.icon) || Object.keys(selectedDevices).includes(booster.icon)
+
+      if (foundInAppliedModifiers) {
+         setIsBolcked(!foundInAppliedModifiers)
+      } else {
+         setIsBolcked(true)
+         deviceDispatch({
+            type: 'REMOVE_BATTLE_BOOSTER_MODIFIER',
+            payload: {
+               archeType: booster.icon as BattleBoosterModifierKeys,
+            },
+         })
+         setISSelected(true)
+      }
+   }, [incompatibleDevices, booster.icon, selectedDevices, deviceDispatch])
+
+   function AddRemoveBooster() {
+      if (booster.modifiers) {
+         if (
+            appliedBattleBoosterModifiers &&
+            (Object.keys(appliedBattleBoosterModifiers).includes(booster.icon) ||
+               Object.keys(appliedBattleBoosterModifiers).includes(booster.name))
+         ) {
+            deviceDispatch({
+               type: 'REMOVE_BATTLE_BOOSTER_MODIFIER',
+               payload: {
+                  archeType: booster.icon as BattleBoosterModifierKeys,
+               },
+            })
+            if (booster.icon === 'improvedVentilation') {
+               deviceDispatch({
+                  type: 'REMOVE_BATTLE_BOOSTER_MODIFIER',
+                  payload: {
+                     archeType: 'improvedVentilationBattleBooster',
+                  },
+               })
+               crewDispatch({
+                  type: 'REMOVE_APPLIED_CREW_MODIFIER',
+                  payload: 'improvedVentilationBattleBooster',
+               })
+            }
+            setISSelected(true)
+         } else {
+            if (booster.icon === 'improvedVentilation') {
+               deviceDispatch({
+                  type: 'SET_BATTLE_BOOSTER_MODIFIER',
+                  payload: {
+                     archeType: 'improvedVentilationBattleBooster',
+                     name: booster.modifiers[0].name,
+                     value: booster.modifiers[0].value,
+                  },
+               })
+            } else {
+               booster.modifiers.forEach((modifier) => {
+                  deviceDispatch({
+                     type: 'SET_BATTLE_BOOSTER_MODIFIER',
+                     payload: {
+                        archeType: booster.icon as BattleBoosterModifierKeys,
+                        name: modifier.name,
+                        value: modifier.value,
+                     },
+                  })
+               })
+            }
+            setISSelected(false)
+         }
+      }
+   }
+
+   return {
+      isBlocked,
+      isSelected,
+      AddRemoveBooster,
+   }
+}

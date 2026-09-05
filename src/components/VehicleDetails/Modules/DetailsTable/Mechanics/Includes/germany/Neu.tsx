9ffe9a256d150d009cfe950b7d87023b0195a0e5
nagytommy76 <nagytommy76@gmail.com>
2026-09-05 17:30:37 +0200

    Finish Neu line mechanics

--- src/components/VehicleDetails/Modules/DetailsTable/Mechanics/Includes/germany/Neu.tsx
+++ src/components/VehicleDetails/Modules/DetailsTable/Mechanics/Includes/germany/Neu.tsx
@@ -1,5 +1,13 @@
 import TableRowComponent from '../../../Includes/TableRow'
+import type { NeuLineMechanics } from '../../Types'
 
-export default function Neu() {
-   return <div>Neu</div>
+export default function Neu({ mechanics }: { mechanics: NeuLineMechanics }) {
+   return (
+      <TableRowComponent
+         iconSrc='/icons/mechanics/borken/designatorInitialCooldownS.png'
+         titleText='Time To Switch Modes'
+         valueText={mechanics.mechanics.shellParamsSwitchingTime}
+         unit='seconds'
+      />
+   )
 }

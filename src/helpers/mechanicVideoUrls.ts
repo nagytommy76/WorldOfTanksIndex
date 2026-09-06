05fe4d21a652b69c807aa09925666240eaa89b83
nagytommy76 <nagytommy76@gmail.com>
2026-09-06 17:18:10 +0200

    Add YouTube urls for mechanic videos

---
+++ src/helpers/mechanicVideoUrls.ts
@@ -0,0 +1,73 @@
+const mechanicUrls: { [tank_name: string]: string } = {
+   /**
+    * @description GERMAN VEHICLES ----------------------------------------
+    */
+   G197_Pz_Kpfw_Neu: 'https://youtu.be/VUlX_h0dc0A?si=p4hl-9fkmtOZdPvr',
+   G198_Kampfpanzer_67: 'https://youtu.be/VUlX_h0dc0A?si=p4hl-9fkmtOZdPvr',
+   G196_Versuchspanzer_57: 'https://youtu.be/VUlX_h0dc0A?si=p4hl-9fkmtOZdPvr',
+   G193_Pz_Kpfw_55: 'https://youtu.be/VUlX_h0dc0A?si=p4hl-9fkmtOZdPvr',
+   G189_Hirschkafer: 'https://youtu.be/_QFDqxgtSqc?si=WMnMYdP6jOCoe2y9',
+   G187_Taschenratte: 'https://youtu.be/2nW1U2ElPkY?si=Q5ca-wJh-9k8YBGn',
+   G188_LeKpz_Borkenkafer: 'https://youtu.be/3swAqkRuhXM?si=nEUNuxc428t-UaeL',
+   G185_Leopard_120_Verbessert: 'https://youtu.be/ZTqCcRxZusY?si=ZsD-mVWrahRGMoNM',
+   /**
+    * @description U.S.S.R VEHICLES ----------------------------------------
+    */
+   R228_KR_1: 'https://youtu.be/hbSAHgFBexk?si=2Zt91RnMIotKq3gK',
+   R230_Object_432U: 'https://youtu.be/wB1U1aJS7LA?si=mjoweV7cM7bXfhJO',
+   /**
+    * @description USA VEHICLES ----------------------------------------
+    */
+   A195_Gorilla: 'https://youtu.be/pV3tWOW9HH4?si=bG36EwJQM_3l78z5',
+   A182_T803: 'https://youtu.be/VjKNOPQye90?si=F10U1tkcSthqN0MF',
+   A183_XM69_Hacker: 'https://youtu.be/45W4Um1xNDk?si=eYTl24Rk7zLpDhr-',
+   A191_Ares_90_C: 'https://youtu.be/oHAtbzCM1_A?si=krjdBylX12qZSahp',
+   A189_Ares_90: 'https://youtu.be/oHAtbzCM1_A?si=krjdBylX12qZSahp',
+   A190_Ares_85: 'https://youtu.be/oHAtbzCM1_A?si=krjdBylX12qZSahp',
+   A187_Ares_75: 'https://youtu.be/oHAtbzCM1_A?si=krjdBylX12qZSahp',
+   /**
+    * @description FRANCE VEHICLES ----------------------------------------
+    */
+   F136_AMX_67_Imbattable: 'https://youtu.be/6r1H2AqMQZg?si=GmtqCWmgrs4UIGTk',
+   F143_Fauteur: 'https://youtu.be/bsV0U_L7AUA?si=NjfbQT3yDeSSNCKZ',
+   F135_AS_XX_40_t: 'https://youtu.be/WtwYUDMxe2E?si=59x9Ys6gpZLDhbfF',
+   /**
+    * @description UK VEHICLES ----------------------------------------
+    */
+   GB152_AT_FV230_Breaker: 'https://youtu.be/z8wxnRQP9cc?si=vKYYaYW3RCz9PvrT',
+   GB147_FV4025_Contriver: 'https://youtu.be/3wHDhjpIHRw?si=KHQqr8ESX7r8aMAm',
+   GB158_Executor: 'https://youtu.be/lTqKC2W-7qM?si=KBJyF-htVrU4QZPz',
+   /**
+    * @description CHINA VEHICLES ----------------------------------------
+    */
+   Ch70_PTZ_78: 'https://youtu.be/EdRKXMsM_8Y?si=43pDTQD_sO14jJSK',
+   Ch67_BZ_79: 'https://youtu.be/oCHD86Qg7M8?si=UV_LtoxX_pqW2VHQ',
+   Ch71_WZ_219: 'https://youtu.be/hU0IvUh3ftI?si=V_wf6ypVLRlhEZ1t',
+
+   /**
+    * @description UK VEHICLES ----------------------------------------
+    */
+   J53_Ho_Ri_Shugo: 'https://youtu.be/5tEykPMFNcw?si=7qkaPH0BhfSkslZ6',
+   J52_STK_2: 'https://youtu.be/9a9wxjeMiCw?si=4m81wnquOSoWJROW',
+
+   /**
+    * @description CHECZ VEHICLES ----------------------------------------
+    */
+   Cz46_Vz_63P: 'https://youtu.be/640fR4WKYi4?si=4Iew9k5U5chWyZTn',
+
+   /**
+    * @description POLISH VEHICLES ----------------------------------------
+    */
+   Pl37_CS_67_Szakal: 'https://youtu.be/RlCR-jtPxTI?si=RJCJYebjOvZL1qIB',
+   /**
+    * @description SWEDISH VEHICLES ----------------------------------------
+    */
+   S36_Strv_107_12: 'https://youtu.be/8AzlkIh7xbE?si=rwjTyRm3dA__M4xc',
+   S41_BV_111: 'https://youtu.be/By5UEamb8Aw?si=Y2dWmbzBSf-eraNq',
+   /**
+    * @description ITALIAN VEHICLES ----------------------------------------
+    */
+   It43_CAV_mod_71: 'https://www.youtube.com/watch?v=Six3NhFeK5Q',
+}
+
+export default mechanicUrls
